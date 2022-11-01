8f46758451a98f5f6e5ba7d3d5966a41d3892189
ClintNunez <clintnunez14@gmail.com>
2022-11-01 20:33:32 +0800

    added header file for binary conversion

---
+++ c++/Binary/custom_binary_converter.h
@@ -0,0 +1,8 @@
+#if !defined(CUSTOM_BINARY_CONVERTER_H)
+#define CUSTOM_BINARY_CONVERTER_H
+
+namespace custom_binary_converter {
+    // use the method of dividing the decimal number with 2 and getting the remainder
+}
+
+#endif
