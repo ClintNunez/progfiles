ba68fb0f6d7bf529a45bd42f49dc6658794520f4
ClintNunez <clintnunez14@gmail.com>
2022-11-01 16:05:34 +0800

    progress exercism

---
+++ exercism/to_solve/gradeschool/grade_school.h
@@ -0,0 +1,11 @@
+#if !defined(GRADE_SCHOOL_H)
+#define GRADE_SCHOOL_H
+
+#include <map>
+
+namespace grade_school {
+
+
+}
+
+#endif
---
+++ exercism/to_solve/gradeschool/main.cpp
@@ -0,0 +1,8 @@
+#include "grade_school.h"
+#include <iostream>
+#include <map>
+
+int main() {
+
+    return 0;
+}
