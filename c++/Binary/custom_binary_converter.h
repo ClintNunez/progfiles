266aa87b0470884bd0c5606cc100aab1886bbbe6
ClintNunez <clintnunez14@gmail.com>
2022-11-02 18:45:56 +0800

    progress in header file

--- c++/Binary/custom_binary_converter.h
+++ c++/Binary/custom_binary_converter.h
@@ -1,8 +1,59 @@
 #if !defined(CUSTOM_BINARY_CONVERTER_H)
 #define CUSTOM_BINARY_CONVERTER_H
 
+#include <string>
+#include <iostream>
+
 namespace custom_binary_converter {
-    // use the method of dividing the decimal number with 2 and getting the remainder
+    const int binaryValues[] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+    class converter {
+        private:
+            int binaryLength, decimal = 0;
+            std::string binary = "";
+            std::string::iterator stringIt;
+            int bit, quotient = 0;
+
+        public:
+            converter() {}
+        
+            // not needed to be a return type of string
+            std::string binary_to_decimal(std::string input_binary) {
+                binaryLength = input_binary.length() - 1;
+                
+                // will only work when 9 < binaryLength < 19
+                if(binaryLength > 9) {
+                    decimal += 512 * binaryValues[binaryLength - 9];   
+                    binaryLength = binaryLength - (binaryLength - 9);
+                
+                    // idk if this is a good solution
+                    input_binary = input_binary.substr(1,input_binary.length() + 1);
+                }
+
+                for(int i = 0; i < 10; i++) {
+                    if(binary[i] == '1') {
+                        std::cout << "reached" << std::endl;
+                        decimal += binaryValues[binaryLength - i];
+                    }
+                }
+
+                return std::to_string(decimal);
+            }
+            
+
+            std::string decimal_to_binary(int decimal) {
+                quotient = decimal;
+                while(quotient != 0) {
+                    bit = quotient % 2;
+                    quotient = quotient / 2;
+
+                    binary.insert(0, std::to_string(bit));
+                }
+                return binary;
+            }
+
+    };
+
 }
 
 #endif
--- c++/Binary/main.cpp
+++ c++/Binary/main.cpp
@@ -1,48 +1,64 @@
-#include<iostream>
-#include<vector>
-#include<string>
-
-std::vector<int> binary;
-
-// TODO Use class after
-// search the benefits of inline function
-
-int binaryToDecimal(){
-	
-	return 0;
-}
-
-const std::string ACTIONS[] = {
-	"1) Binary to Decimal"
-};
+#include "custom_binary_converter.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
 // initial solution for getting the binary input (string => each char to int => binary.push_back(each char)
+
+void greeting_text() {
+    std::cout << "Custom Binary Converter by Clint Patrick M. Nunez \n" << std::endl;
+}
+
+
+
 int main(){
+    greeting_text();
+
 	char choice;
-	std::string binaryInput; int binaryInputSize;
-	
+    int actionChoice;
+	custom_binary_converter::converter Converter;
+
+    std::string binary;
+    int decimal; 
+
 	do{
-		std::cout << "Enter a Binary number: ";
-		std::cin >> binaryInput;
-		binaryInputSize = binaryInput.std::string::size();
 
-		for(int i = 0; i < binaryInputSize; i++){
-			binary.push_back(binaryInput[i]); // convert char to int
-		}
+        std::cout << "What do you want to do?: " << std::endl;
+        std::cout << "1) Convert binary to decimal." << std::endl;
+        std::cout << "2) Convert decimal to binary." << std::endl;
+        std::cin >> actionChoice; 
+        std::cout << std::endl;
 
-		for(std::vector<int>::iterator eachBit = binary.begin(); eachBit != binary.end(); eachBit++){
-			std::cout << *eachBit;
-		}
+        switch(actionChoice) {
+            case 1: 
+                // TODO add a do while loop
+                std::cout << "Enter binary: ";
+                std::cin >> binary;
 
-		std::cout << "Input another binary(y/n)? "; // Check if this is better or just use a sentinal value
+                std::cout << "Binary: " << binary << std::endl;
+                std::cout << "Decimal: " << Converter.binary_to_decimal(binary) << std::endl;
+                break;
+            case 2:
+                std::cout << "Enter decimal: ";
+                std::cin >> decimal;
+            
+                std::cout << "Decimal: " << decimal << std::endl;
+                std::cout << "Binary: " << Converter.decimal_to_binary(decimal) << std::endl;
+                break;
+        }
+        
+
+		std::cout << "Run the again(y/n)? "; // Check if this is better or just use a sentinal value
 		std::cin >> choice; // convert it to small case
-		
-		while(choice != 'y' or choice != 'n'){
+	     	
+		while(choice != 'y' && choice != 'n'){
 			std::cout << "Choose between y/n only: ";
 			std::cin >> choice;
 		}		
 
 	}while(choice != 'n');
+
+    std::cout << "Exiting Custom Binary Converter." << std::endl;
 
 	return 0;
 }
