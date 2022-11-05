84c76eb6bf924dfc924c196b7ad578db03151bec
ClintNunez <clintnunez14@gmail.com>
2022-11-05 18:56:55 +0800

    progress in exercism

--- exercism/to_solve/proteintranslation/protein_translation.h
+++ exercism/to_solve/proteintranslation/protein_translation.h
@@ -1,10 +1,10 @@
 #if !defined(PROTEIN_TRANSLATION_H)
 #define PROTEIN_TRANSLATION_H
 
-#include<vector>
-#include<string>
-#include<stdexcept>
-#include<iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
 namespace protein_translation {
     std::vector<std::string> proteins(std::string RNA) {
         std::vector<std::string> translated;
--- exercism/to_solve/sieve/sieve.h
+++ exercism/to_solve/sieve/sieve.h
@@ -5,22 +5,22 @@
 #include<vector>
 
 namespace sieve {
-    std::vector<int> primes(int limit) {
-        bool pMarks[limit + 1];
-        memset(pMarks, true, sizeof(pMarks));
+    inline std::vector<int> primes(int limit) {
+        std::vector<int> pMarks(limit + 1);
+        std::fill(pMarks.begin(), pMarks.end(), true);
 
         std::vector<int> listOfPrimes;
         
         for(int p = 2; p * p <= limit; p++) { // get start numbers
-            if(pMarks[p]) {
+            if(pMarks.at(p)) {
                 for(int i = p * p; i <= limit; i += p) {
-                    pMarks[i] = false;
+                    pMarks.at(i) = false;
                 }
             }
         }
         
         for(int i = 2; i <= limit; i++) {
-            if(pMarks[i]) {
+            if(pMarks.at(i)) {
                 listOfPrimes.push_back(i);
             }
         }
