eab281d759791797c1018f0810c852a702f28de0
ClintNunez <clintnunez14@gmail.com>
2022-11-10 14:50:12 +0800

    progress in exercism

--- exercism/solved/scrabblescore/scrabble_score.h
+++ exercism/solved/scrabblescore/scrabble_score.h
@@ -13,20 +13,19 @@
  *
  */
 
-
-
 namespace scrabble_score {
 
     inline int score(std::string word) {
         std::map<std::vector<char>, int> letterScores = {
-            {{}, 1},
-            {{}, 1},
-            {{}, 1},
-            {{}, 1},
-            {{}, 1},
-            {{}, 1},
-            {{}, 1},
+            {{'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'}, 1},
+            {{'D', 'G'}, 2},
+            {{'B', 'C', 'M', 'P'}, 3},
+            {{'F', 'H', 'V', 'W', 'Y'}, 4},
+            {{'K'}, 5},
+            {{'J', 'X'}, 8},
+            {{'Q', 'Z'}, 10},
         }; 
+
         std::map<std::vector<char>, int> iterator;
 
         int word_length = word.length(), score = 0;
@@ -36,8 +35,9 @@ namespace scrabble_score {
             for(auto eachScores = letterScores.begin(); eachScores != letterScores.end(); eachScores++) {
                 std::vector<char> letters = eachScores->first;
                 for(auto eachLetter = letters.begin(); eachLetter != letters.end(); eachLetter++) {
-                    if(word[i] == *eachLetter) {
+                    if(toupper(word[i]) == *eachLetter) {
                         score += eachScores->second;
+                        break;
                     }
                 }
             }
