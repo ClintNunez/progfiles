9d4d5cfce0c425d66f035811cb147ef6c76507c4
ClintNunez <clintnunez14@gmail.com>
2022-11-12 22:37:07 +0800

    progress in spiral

--- c++/Cartesian/main.cpp
+++ c++/Cartesian/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 // CURRENT TODO
@@ -16,13 +17,12 @@ std::vector<std::pair<int, int>> points;
 
 const char XAXIS_LEFTEND = '<', XAXIS_LINE = '-', XAXIS_RIGHTEND = '>',
             YAXIS_TOPEND = '^', YAXIS_LINE = '|', YAXIS_BOTTOMEND = 'v',
-            ORIGIN = 'o', Point = 'O', SPACE = ' ';
+            ORIGIN_SIGN = 'o', Point = 'O', SPACE = ' ';
 
 void printCartesian() {
-    for(auto each_row = cartesian_plane.begin(); each_row != cartesian_plane.end(); each_row++) {
-        std::vector<char> row = *each_row; // check if this is needed
-        for(auto each_element = row.begin(); each_element != row.end(); each_element++) {
-            std::cout << each_element;
+    for(int i = 0; i < cartesian_plane.size(); i++) {
+        for(int j = 0; j < cartesian_plane[i].size(); j++) {
+            std::cout << std::setw(2) << cartesian_plane[i][j];
         }
         std::cout << std::endl;
     }
@@ -35,13 +35,29 @@ void makeCartesianPlane(int size_of_side) {
     const int ORIGIN_POS = size_of_cartesian / 2;
     
     for(int y = 0; y < size_of_cartesian; y++) {
+        std::vector<char> each_row;
         for(int x = 0; x < size_of_cartesian; x++) {
-            if(x == ORIGIN_POS && y == (ORIGIN_POS - (size_of_side + 1)) {
-                cartesian_plane.push_back(YAXIS_TOPEND);
-            } else if() {
-
+            if(x == ORIGIN_POS && y == ORIGIN_POS) {
+                each_row.push_back(ORIGIN_SIGN);
+            } else if(x == ORIGIN_POS && y == (ORIGIN_POS - (size_of_side + 1))) {
+                each_row.push_back(YAXIS_TOPEND);
+            } else if(x == ORIGIN_POS && (y > 0 && y < (size_of_cartesian - 1))) {
+                each_row.push_back(YAXIS_LINE);
+            } else if(x == ORIGIN_POS && y == (ORIGIN_POS + (size_of_side + 1))) {
+                each_row.push_back(YAXIS_BOTTOMEND);
+            } else if(x == (ORIGIN_POS - (size_of_side + 1)) && y == ORIGIN_POS) {
+                std::cout << "reached" << std::endl;
+                each_row.push_back(XAXIS_LEFTEND);
+            } else if((x > 0 && x < (size_of_cartesian - 1)) && y == ORIGIN_POS) {
+                each_row.push_back(XAXIS_LINE);
+            } else if(x == (ORIGIN_POS + (size_of_side + 1)) && y == ORIGIN_POS) {
+                each_row.push_back(XAXIS_RIGHTEND);
+            } else {
+                each_row.push_back(SPACE);
             }
         }
+        std::cout << std::endl;
+        cartesian_plane.push_back(each_row);
     }
     
     printCartesian();
@@ -66,7 +82,7 @@ int main() {
     std::cout << "\n\tEnter size of Cartesian Plane: "; //reword
     int size_of_cartesian; std::cin >> size_of_cartesian;
 
-    size_of_cartesian++; // adds an offset for edge characters: '<' & '>'
+    size_of_cartesian; // adds an offset for edge characters: '<' & '>'
 
     makeCartesianPlane(size_of_cartesian);
 
