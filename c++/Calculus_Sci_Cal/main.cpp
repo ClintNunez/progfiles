307883998c0d76516a0d92192ce4cfdb6b89890f
ClintNunez <clintnunez14@gmail.com>
2022-11-14 20:10:53 +0800

    initial commit

---
+++ c++/Calculus_Sci_Cal/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+const double PI = 3.14159265;
+
+int main() {
+    int choice;
+    do {
+        std::cout << "Enter degrees: ";
+        double degrees; std::cin >> degrees;
+        
+        degrees = degrees * PI / 180.0;
+    
+        std::cout << "sin: " << sin(degrees) << std::endl;
+        std::cout << "cos: " << cos(degrees) << std::endl;
+        std::cout << "tan: " << tan(degrees) << std::endl;
+        std::cout << "csc: " << 1/sin(degrees) << std::endl;
+        std::cout << "sec: " << 1/cos(degrees) << std::endl;
+        std::cout << "cot: " << 1/tan(degrees) << std::endl;
+
+        std::cout << "Enter 0 to exit: " << std::endl;
+        std::cin >> choice;
+
+    } while(choice != 0);
+
+    return 0;
+}
