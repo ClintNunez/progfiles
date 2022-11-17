ef6fe5a59b8151a0e12a97d0085eb54932aefd24
ClintNunez <clintnunez14@gmail.com>
2022-11-17 22:07:41 +0800

    finished graph values

--- c++/Calculus_Sci_Cal/main.cpp
+++ c++/Calculus_Sci_Cal/main.cpp
@@ -3,15 +3,13 @@
 #include <string>
 
 // CURRENT TODO
-// TODO add conversion option after computing trigonometric functions
-// TODO add formula for the last pages in the module
-// TODO allow inputs for radians like 2/3 pi
-// TODO add formulas for getting period etc
 // TODO optimize to use global variabls or addresses.
 // TODO add loops for how many inputs to calculate to all functions
 // TODO add loops for how many inputs to calculate to all functions
 
 // FINISHED TODO
+// TODO allow inputs for radians like 2/3 pi
+// TODO add formulas for getting period etc
 // TODO separate code into functions
 // TODO add proper prompts
 
@@ -24,11 +22,11 @@ std::pair<int, int> Get_Pair_From_Fraction(std::string fraction, int div_pos) {
     std::pair<int, int> values;
 
     if(div_pos != std::string::npos) {
-        int numerator = std::stod(fraction.substr(0, div_pos));
-        int denominator = std::stod(fraction.substr(div_pos + 1, (int) fraction.length() - 1));
+        int numerator = std::stoi(fraction.substr(0, div_pos));
+        int denominator = std::stoi(fraction.substr(div_pos + 1, (int) fraction.length() - 1));
         values = std::pair(numerator, denominator);
     } else {
-        int digit = std::stod(fraction);
+        int digit = std::stoi(fraction);
         values = std::pair(digit, 1);
     }
 
@@ -78,7 +76,7 @@ void Radians_Trigonometric_Functions() {
 
 void Graph_Values_Cos_Sin() {
     std::cout << "\tAmplitude, Horizontal/Phase shift, vertical shift and period for sin and cos." << std::endl;
-    std::cout << "\tInput format:" << std::endl;
+    std::cout << "\n\tInput format:" << std::endl;
     std::cout << "\ta => put positive or negative signs, doesn't matter." << std::endl;
     std::cout << "\tFor b:" << std::endl;
     std::cout << "\t   if b is a number, input: number n. ex: 3 n. " << std::endl;
@@ -90,8 +88,13 @@ void Graph_Values_Cos_Sin() {
     std::cout << "\tFor c:" << std::endl;
     std::cout << "\t   if c is a pi, input 1 pi" << std::endl;
     std::cout << "\t   if c is fraction times pi, input: numerator/denominator pi. ex: 1/2 pi" << std::endl;
+    std::cout << "\t   if c does not exist, input: 0 n" << std::endl;
+    std::cout << "\t   Note: c seems to always have a pi." << std::endl;
     std::cout << "\td => input as is. ex: -2" << std::endl;
-    std::cout << "\tEnter a, b, c, d: "; 
+    std::cout << "\n\tSample Complete input:" << std::endl;
+    std::cout << "\t3 1/2 pi 1 n 0 | These inputs means that a = 3/-3, b = 1/2 pi, c = 0 or does not exist, d = 0 or does not exist." << std::endl;
+    std::cout << "\n\tEnter a, b, c, d: "; 
+
     int a, b1, b2, c1, c2, d;
     std::string b_fraction, c_fraction, b_pi, c_pi;
     std::cin >> a >> b_fraction >> b_pi >> c_fraction >> c_pi >> d; 
@@ -103,13 +106,13 @@ void Graph_Values_Cos_Sin() {
 
     std::string period;
     if(b_div_pos != std::string::npos) { // for fractions
-        if((2 * b2) % b1 == 0.0) {
+        if((2 * b2) % b1 == 0) {
             period.append(std::to_string((2 * b2) / b1)); 
         } else {
             period.append((2 * b2) + "/" + b1); 
         }
 
-        if(b_pi == "pi") {
+        if(b_pi != "pi") {
             period.append(" pi");
         }     
     } else { // for numbers
@@ -117,8 +120,10 @@ void Graph_Values_Cos_Sin() {
             period.append(std::to_string(2 / b1)); 
         } else if(b1 % 2 == 0){
             period.append("1/" + std::to_string(b1 / 2)); 
+        } else {
+            period.append("2/" + std::to_string(b1));
         }
-
+        
         if(b_pi != "pi") {
             period.append(" pi");
         }     
@@ -130,36 +135,56 @@ void Graph_Values_Cos_Sin() {
     c1 = result.first; c2 = result.second;
 
     std::string phase_shift;
-    if(c_div_pos != std::string::npos && b_div_pos != std::string::npos) { // for b and c are fractions
-
-
-        if((b_pi == "pi" && c_pi != "pi") || (b_pi != "pi" && c_pi == "pi")) { 
-            period.append(" pi");
-        }     
-    } else { // for numbers
-        if(2 % b1 == 0) {
-            period.append(std::to_string(2 / b1)); 
-        } else if(b1 % 2 == 0){
-            period.append("1/" + std::to_string(b1 / 2)); 
+    if(c1 != 0) {
+        if(c_div_pos != std::string::npos && b_div_pos != std::string::npos) { // for b and c are fractions
+            if((c1 * b2) % (c2 * b1) == 0) {
+                phase_shift.append(std::to_string((c1 * b2) / (c2 * b1))); // numerator: (c1 * b2), denominator: (c2 * b1)
+            } else if((c2 * b1) % (c1 * b2) == 0){
+                phase_shift.append("1/" + ((c2 * b1) / (c1 * b2)));
+            } else {
+                phase_shift.append((c1 * b2) + "/" + (c2 * b1));
+            }
+        } else if(c_div_pos != std::string::npos && b_div_pos == std::string::npos){ // for b is a number and c is a fraction
+            if(c1 % (c2 * b1) == 0) {
+                phase_shift.append(std::to_string(c1 / (c2 * b1)));
+            } else if((c2 * b1) % c1 == 0){
+                phase_shift.append("1/" + std::to_string((c2 * b1) / c1));
+            } else {
+                phase_shift.append(std::to_string(c1) + "/" + std::to_string(c2 * b1));
+            }
+        } else if(c_div_pos == std::string::npos && b_div_pos != std::string::npos) { // for b is a fraction and c is a number
+            if(b1 % (b2 * c1) == 0) {
+                phase_shift.append(std::to_string(b1 / (b2 * c1)));
+            } else if((b2 * c1) % b1 == 0){
+                phase_shift.append("1/" + std::to_string((b2 * c1) / b1));
+            } else {
+                phase_shift.append(std::to_string(b1) + "/" + std::to_string(b2 * c1));
+            }
+        } else { // b and c are numbers
+            if(c1 % b1 == 0) {
+                phase_shift.append(std::to_string(c1 / b1));
+            } else if(b1 % c1 == 0){
+                phase_shift.append("1/" + std::to_string(b1 / c1));
+            } else {
+                phase_shift.append(std::to_string(b1) + "/" + std::to_string(c1));
+            }
         }
-
-        if(b_pi == "pi") {
-            period.append(" pi");
+        
+        if((b_pi == "pi" && c_pi == "n") || (b_pi == "n" && c_pi == "pi")) { // since c always have a pi
+            phase_shift.append(" pi");
         }     
+    } else {
+        phase_shift = "0";
     }
 
     std::cout << "\t\tAmplitude: " << abs(a) << std::endl;
     std::cout << "\t\tPeriod: " << period << std::endl;
     std::cout << "\t\tHorizontal/Phase shift: " << phase_shift << std::endl;
     std::cout << "\t\tVertical shift: " << d << std::endl;
+    std::cout << "\t\tNote: if the result of the fraction is not simplified, please simplify it on your own.\n\t\tI can\'t add that feature due to time constraints." << std::endl;
 }
 
-void Graph_Values_Tan() {
-    
-}
-
-
-/*Functions for getting formulas*/
+/*Function for getting formulas*/
 void All_Formula() {
     std::cout << "\tArc Length" << std::endl;
     std::cout << "\ts = rt, where r is the radius and t is the " << std::endl;
@@ -172,7 +197,8 @@ void Choice_Prompt() {
     std::cout << "\t(2) Trigonometric Functions in Radians" << std::endl;
     std::cout << "\t(3) Convert Radians to Degrees" << std::endl;
     std::cout << "\t(4) Convert Degrees to Radians" << std::endl;
-    std::cout << "\t(5) Show all formulas" << std::endl;
+    std::cout << "\t(5) Graph Values for Sine and Cosine" << std::endl;
+    std::cout << "\t(6) Show all formulas" << std::endl;
     std::cout << "\t(0) Exit" << std::endl;
     std::cout << "=======================================================" << std::endl;
     std::cout << "\t> ";
@@ -211,6 +237,10 @@ int main() {
                     flag = false;
                     break;
                 case 5:
+                    Graph_Values_Cos_Sin();
+                    flag = false;
+                    break;
+                case 6:
                     All_Formula();
                     flag = false;
                     break;
