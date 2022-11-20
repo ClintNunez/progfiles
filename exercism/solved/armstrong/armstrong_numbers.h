a8f14ef20c122585dff432026f8ebae369b41107
ClintNunez <clintnunez14@gmail.com>
2022-11-20 16:51:13 +0800

    optimized armstrong a bit

--- exercism/solved/armstrong/armstrong_numbers.h
+++ exercism/solved/armstrong/armstrong_numbers.h
@@ -2,18 +2,12 @@
 #define ARMSTRONG_NUMBERS_H
 
 #include <math.h>
+#include <string>
 
-// TODO optimize
 namespace armstrong_numbers {
     inline bool is_armstrong_number(int num) {
-        int length = 0, tempnum = num, result = 0;
         
-        while(tempnum != 0) {
-            tempnum /= 10;
-            length++;
-        }
-
-        tempnum = num;
+        int length = std::to_string(num).length(), tempnum = num, result = 0;
 
         for(int i = length - 1; i >= 0; i--) {
             result += pow((tempnum / (int) pow(10, i)), length);   
