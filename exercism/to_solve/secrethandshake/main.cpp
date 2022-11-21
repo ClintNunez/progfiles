67c70caca14276c24e22b4f6c84029ed4b2963f3
ClintNunez <clintnunez14@gmail.com>
2022-11-21 16:15:59 +0800

    finished secrethandshake

--- exercism/to_solve/secrethandshake/main.cpp
+++ exercism/to_solve/secrethandshake/main.cpp
@@ -4,7 +4,15 @@
 #include <vector>
 
 int main() {
+    std::cout << "Enter decimal for secret handshake: ";
+    int decimal; std::cin >> decimal;
 
+    std::vector<std::string> result = secret_handshake::commands(decimal);
+        
+    std::vector<std::string>::iterator each_element;
+    for(each_element = result.begin(); each_element != result.end(); each_element++) {
+        std::cout << *each_element << std::endl;
+    }
 
     return 0;
 }
--- exercism/to_solve/secrethandshake/secret_handshake.h
+++ exercism/to_solve/secrethandshake/secret_handshake.h
@@ -5,9 +5,42 @@
 #include <vector>
 
 namespace secret_handshake {
-    std::vector<std::string> commands(int command) {
+    std::vector<std::string> commands(int decimal) {
         std::vector<std::string> translated;
 
+        if(decimal != 16) {
+            std::string handshake[] = {"wink", "double blink", "close your eyes", "jump"};
+                
+            std::string binary = "";
+            int quotient = decimal, remainder;
+            while(quotient != 0) {
+                remainder = quotient % 2;
+                quotient /= 2;
+
+                binary.insert(0, std::to_string(remainder));
+            }
+
+            int binary_length = binary.length() - 1;
+            
+            if(binary_length > 4) {
+                binary.substr(0, binary_length - 4);
+                binary_length = binary.length();
+            }
+            
+            for(int i = binary_length; i >= 0; i--) {
+                if(binary[i] == '1') {
+                    if(decimal > 16) {
+                        if(!translated.empty()) {
+                            translated.insert(translated.begin(), handshake[binary_length - i]);
+                        } else {
+                            translated.push_back(handshake[binary_length - i]);
+                        }
+                    } else {
+                        translated.push_back(handshake[binary_length - i]);
+                    }
+                } 
+            }
+        }
         return translated;
     }
 }
