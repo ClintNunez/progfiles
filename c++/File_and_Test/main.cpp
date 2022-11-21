a77c3ef67290098f6a276d174ef77983949a09ad
ClintNunez <clintnunez14@gmail.com>
2022-11-21 16:37:25 +0800

    initial commit

---
+++ c++/File_and_Test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// CURRENT TODO
+// TODO add class for getting files
+// TODO do all basic actions that you can do on files
+
+// FINISHED TODO
+//
+
+int main() {
+
+    return 0;
+}
