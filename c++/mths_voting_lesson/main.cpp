c708d81a5599912891d74b85786bfa7b856cf67d
ClintNunez <clintnunez14@gmail.com>
2022-11-21 16:34:44 +0800

    setup mths voting prog

---
+++ c++/mths_voting_lesson/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//CURRENT TODO
+//TODO add functions for all methods of voting
+//TODO use call by reference in these functions to preserve memory
+
+//FINISHED TODO
+//
+
+int main() {
+
+
+    return 0;
+}
