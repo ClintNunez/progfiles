f3acd1ce8ee01a219e1cf443bfee1dd16a72006d
ClintNunez <clintnunez14@gmail.com>
2022-11-21 16:28:34 +0800

    progress in pascal's triangle

--- exercism/to_solve/pascaltriangle/main.cpp
+++ exercism/to_solve/pascaltriangle/main.cpp
@@ -1,7 +1,22 @@
 #include "pascals_triangle.h"
 #include <iostream>
+#include <vector>
 
 int main() {
+    std::cout << "Enter rows for pascal's triangle: ";
+    int rows; std::cin >> rows;
+
+    std::vector<std::vector<int>> result = pascals_triangle::generate_rows(rows);
+
+    std::vector<std::vector<int>>::iterator each_row;
+    for(each_row = result.begin(); each_row != result.end(); each_row++) {
+        std::vector<int> row = *each_row;
+        std::cout << "{";
+        for(std::vector<int>::iterator each_element = row.begin(); each_element != row.end(); each_element++) {
+            std::cout << *each_element << ", ";
+        }
+        std::cout << "}," << std::endl;
+    }
 
     return 0;
 }
--- exercism/to_solve/pascaltriangle/pascals_triangle.h
+++ exercism/to_solve/pascaltriangle/pascals_triangle.h
@@ -1,8 +1,28 @@
 #if !defined(PASCALS_TRIANGLE_H)
 #define PASCALS_TRIANGLE_H
 
-namespace pascals_triangle {
+#include <vector>
 
+namespace pascals_triangle {
+    inline std::vector<std::vector<int>> generate_rows(int rows) {
+        std::vector<std::vector<int>> triangle;
+        std::vector<int> triangle_row;
+
+        if(rows == 1) {
+            triangle_row.push_back(1);
+            triangle.push_back(triangle_row);
+        } else if(rows == 2) {
+            triangle_row.push_back(1);
+            triangle.push_back(triangle_row);
+            triangle_row.push_back(1);
+            triangle.push_back(triangle_row);
+        } else {
+            // actual code
+
+        }
+
+        return triangle;
+    }
 }
 
 #endif
