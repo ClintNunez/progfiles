e08f32efc346e185a7e8928f94e415e3effa3946
ClintNunez <clintnunez14@gmail.com>
2022-11-21 22:39:25 +0800

    setup

--- exercism/to_solve/nthprime/nth_prime.h
+++ exercism/to_solve/nthprime/nth_prime.h
@@ -3,19 +3,6 @@
 
 namespace nth_prime {
     int nth(int primePos) {
-        bool pFlags[primePos + 1];
-        memset(pFlags, true, sizeof(pFlags));
-
-        // using sieve of eratosthenes is wrong
-        for(int pos = 2; pos * pos <= primePos; pos++) {
-            if(pFlags[pos]) {
-                for(int i = pos * pos; i <= primePos; i += pos) {
-                    pFlags[i] = false;
-                }
-            }
-        }
-
-
 
         return 0;
     }
