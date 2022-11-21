a1ae002f7f3203fc3f1ac1a3cee19e2926f6dd1f
ClintNunez <clintnunez14@gmail.com>
2022-11-21 14:57:50 +0800

    finished luhn

--- exercism/to_solve/luhn/luhn.h
+++ exercism/to_solve/luhn/luhn.h
@@ -3,10 +3,38 @@
 
 #include <string>
 
+#include <iostream>
+
 namespace luhn {
     inline bool valid(std::string id_num) {
+        int length = id_num.length(), digits_ctr = 0, sum = 0, current_digit;
+        
+        if(length != 0) {
+            for(int i = length; i > 0; i--) {
+                if(isdigit(id_num[i - 1])) {
+                    current_digit = (int) id_num[i - 1] - 48;
+                    if(i % 2 != 0) {
+                        current_digit = (current_digit * 2 > 9) ? current_digit * 2 - 9 : current_digit;
+                    }
 
-        return true;
+                    sum += current_digit;
+                } else if(id_num[i] == ' ') {
+                    continue;
+                } else {
+                    return false;
+                }
+
+                digits_ctr++;
+            }
+
+            if(digits_ctr > 1) {
+                if(sum % 10 == 0) {
+                    return true;
+                }                
+            }         
+        } 
+
+        return false;
     }
 }
 
