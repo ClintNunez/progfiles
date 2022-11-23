04466a78878450c963fc76bacc00c576500ce7f2
ClintNunez <clintnunez14@gmail.com>
2022-11-23 17:08:45 +0800

    worked on spiral

--- c++/Spiral_Of_Prime/main.cpp
+++ c++/Spiral_Of_Prime/main.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-#include<vector>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 // CURRENT TODO
@@ -10,19 +11,22 @@ using namespace std;
 
 // FINISHED TODO
 
-// getting prime numbers
+// Global variables
+char hor_path = '-', vert_path = '|', not_prime = 'o', prime = 'O';
+
+// TODO use this later on
 vector<bool> primes;
 void SieveOfEratosthenes(int n) { // Time Complexity: O(n*log(log(n)))
     bool prime[n + 1];
     memset(prime, true, sizeof(prime));
 
-    for(int i = 0; i < n - 1; i++) { // removes one since 1 is not a prime number
+    for (int i = 0; i < n - 1; i++) { // removes one since 1 is not a prime number
         primes.push_back(true);
     }
 
-    for(int p = 2; p * p <= n; p++) {
-        if(primes.at(p)) {
-            for(int i = p*p; i <= n; i += p) {
+    for (int p = 2; p * p <= n; p++) {
+        if (primes.at(p)) {
+            for (int i = p*p; i <= n; i += p) {
                 primes.at(i) = false;
             }
         }
@@ -30,13 +34,24 @@ void SieveOfEratosthenes(int n) { // Time Complexity: O(n*log(log(n)))
 
 }
 
+// getting prime numbers
+bool prime_check(double num) {
+    for (int i = 2; i <= sqrt(num); i++) {
+        if ((int) num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // create the matrix
 vector<vector<char>> m;
 
 void createMatrix(int D) {
-    for(int i = 0; i < D * 3; i++) {
+    m.clear();
+    for(int i = 0; i < D; i++) {
         vector<char> row;
-        for(int j = 0; j < D * 3; j++) {
+        for(int j = 0; j < D; j++) {
             row.push_back(' ');
         }
         m.push_back(row);
@@ -45,63 +60,86 @@ void createMatrix(int D) {
 
 // create spiral
 void createSpiral(int D) {
+    createMatrix(D);
+
     int x, y, direction = 0, repeat = 2, nDim = D * 3;
 
+    // Finds the center of the spiral
     if(D % 2 == 0) {
-        x = (D / 2) + 1; y = (D / 2) + 1;
+        x = (D / 2) - 2; y = (D / 2) + 1;
     } else {
-        x = D / 2; y = D / 2;
+        x = (D / 2); y = (D / 2);
     }
+    
+    int max_step = 1, curr_step = 0, max_reached = 0;
+    int total_moves = (D - 2) * 3 + 4;
+    /*
+     * 0 -> right
+     * 1 -> up
+     * 2 -> left
+     * 3 -> down
+     * */
+    char path = hor_path;
+    int num = 1;
+    int curr_move = 1;
 
-    for(int step = 1; step < D; step++) { // loop for number of steps
-        if(step == (D - 1)) repeat  = 3;
-        for(int j = 0; j < repeat; j++) { // loop for repeating the steps
-            for(int k = 0; k < (step * 3); k++)  { // loop for moving i number of steps
-                switch(direction) {
-                    case 0:
-                        // move up
-                        if(k % 3 == 0) {
-                            // check if it is prime
-                            // if prime set '@' else set '|'
-                        } else {
-                            // assign '|'
-                        }
-                        break;
-                    case 1:
-                        // move left
-                        if(k % 3 == 0) {
-                            // check if it is prime
-                            // if prime set '@' else set '|'
-                        } else {
-                            // assign '-'
-                        }
-                        break;
-                    case 2:
-                        // move down
-                        if(k % 3 == 0) {
-                            // check if it is prime
-                            // if prime set '@' else set '|'
-                        } else {
-                            // assign '|'
-                        }
-                        break;
-                    case 3:
-                        // move right
-                        if(k % 3 == 0) {
-                            // check if it is prime
-                            // if prime set '@' else set '|'
-                        } else {
-                            // assign '-'
-                        }
-                        break;
-
-                }
-                
-                direction = (direction == 3) ? 0 : direction++; // check
-
+    /* For 1st move and last move:
+     *      Have a conditional statement where if i is 1 or num is D
+     *      
+     *      if(i == D) check if num is a prime and stop moving?
+     *      if(i == 1) just put the path
+     * */
+    
+    for(int i = 1; i <= total_moves; i++) {
+        if(curr_move % 4 == 0) {
+            num++;
+            if(prime_check(num)) {
+                m[y][x] = prime;
+            } else {
+                m[y][x] = not_prime;
             }
-            
+            curr_move = 1;
+        } else {
+            m[y][x] = path;
         }
+
+        curr_move++;
+        curr_step++;
+
+        // change direction
+        if(curr_step == max_step) {
+            max_reached++;
+            if(direction == 3) {
+                direction = 0;
+            } else {
+                direction++;
+            }
+            curr_step = 0;
+        }
+        
+        if(max_reached == 2 && max_step < (D / 3) - 1) {
+            max_step++;
+        }
+
+        // move to direction
+        switch(direction) {
+            case 0:
+                x += 1;
+                path = hor_path;
+                break;
+            case 1:
+                y -= 1;
+                path = vert_path;
+                break;
+            case 2:
+                x -= 1;
+                path = hor_path;
+                break;
+            case 3:
+                y += 1;
+                path = vert_path;
+                break;
+       }
 
     }
 }
@@ -121,6 +159,8 @@ int main() {
     cout << "Enter dimensions of matrix: ";
     int numStep; cin >> numStep;
     int dimension = numStep * 3;
+    createSpiral(dimension);
 
-   return 0;
+    printSpiral();
+    return 0;
 }
