c04b683207dfae077f6dc7939f50751cb04b2ff1
ClintNunez <clintnunez14@gmail.com>
2022-12-01 20:02:55 +0800

    isogram setup

---
+++ exercism/to_solve/isogram/isogram.h
@@ -0,0 +1,20 @@
+#if !defined(ISOGRAM_H)
+#define ISOGRAM_H
+
+#include <string>
+
+/* Idea for initial solution 
+ * 
+ * use same code for finding dublicates in the ateneo comp
+ *
+ * */
+
+
+namespace isogram {
+    inline bool is_isogram(std::string word) {
+        
+        return false;
+    }
+}
+
+#endif
---
+++ exercism/to_solve/isogram/main.cpp
@@ -0,0 +1,10 @@
+#include "isogram.h"
+#include <iostream>
+#include <ifstream>
+#include <string>
+
+int main() {
+
+
+    return 0;
+}
