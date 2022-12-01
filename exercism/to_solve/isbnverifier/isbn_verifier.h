e58641dacd76785dbf537cf1ff78fdaed74884f9
ClintNunez <clintnunez14@gmail.com>
2022-12-01 20:16:10 +0800

    isbnverifier setup

---
+++ exercism/to_solve/isbnverifier/isbn_verifier.h
@@ -0,0 +1,13 @@
+#if !defined(ISBN_VERIFIER_H)
+#define ISBN_VERIFIER_H
+
+#include <string>
+
+namespace isbn_verifier {
+    inline bool is_valid(std::string isbn) {
+
+        return true;
+    }
+}
+
+#endif
---
+++ exercism/to_solve/isbnverifier/main.cpp
@@ -0,0 +1,8 @@
+#include "isbn_verifier.h"
+#include <fstream>
+#include <string>
+
+int main() {
+
+    return 0;
+}
