f1a7689100f9cff624681664deec286b3b24329d
ClintNunez <clintnunez14@gmail.com>
2022-12-01 20:23:39 +0800

    binary_search setup

---
+++ exercism/to_solve/binarysearch/binary_search.h
@@ -0,0 +1,14 @@
+#if !defined(BINARY_SEARCH_H)
+#define BINARY_SEARCH_H
+
+#include <vector>
+
+namespace binary_search {
+    inline size_t find(std::vector<int> data, int element) {
+        size_t n;
+
+        return n;
+    }
+}
+
+#endif
---
+++ exercism/to_solve/binarysearch/main.cpp
@@ -0,0 +1,8 @@
+#include "binary_search.h"
+#include <fstream>
+#include <vector>
+
+int main() {
+
+    return 0;
+}
