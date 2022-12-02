db9805f3a076d7bed02ccdf9e3776caff00ad8a4
ClintNunez <clintnunez14@gmail.com>
2022-12-02 16:44:31 +0800

    beersong attempt

--- exercism/to_solve/beersong/beer_song.h
+++ exercism/to_solve/beersong/beer_song.h
@@ -3,47 +3,39 @@
 
 #include <string>
 
-
-/* Initial idea for the solution 
- * for verse if n == 1
- * use the n bottles of beer on the wall, n bottles of beer.
- * and then add the unique string after Take it down and pass it around, no more bottles of beer on the wall.
- * if n == 0
- * string expected = "No more bottles of beer on the wall, no more bottles of beer.\n"
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
- * else 
- * string expected = "n bottles of beer on the wall, n bottles of beer.\n"
-        "Take one down and pass it around, n-1 bottles of beer on the wall.\n";
- *
- * for sing
- * for(int i = start; i >= end; i++) 
- *      if(i == 0)
- *          append 0 string
- *      if(i == 1)
- *          append 1 string
- *      else 
- *          append n string
- *
- */
-
-
-
-
 namespace beer_song {
+
+    const std::string last = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+    const std::string verse_1 = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+
     inline std::string verse(int n) {
         std::string nth_verse;
-
-
+        if(n == 0)
+            nth_verse = last;
+        else if(n == 1) 
+            nth_verse = verse_1;
+        else 
+            nth_verse = std::to_string(n) + " bottles of beer on the wall, " + std::to_string(n) + " bottles of beer.\nTake one down and pass it around, " + std::to_string(n-1) + " bottles of beer on the wall.\n";
+        
         return nth_verse;
     }
 
-    inline std::string sing(int start, int end) {
-        std::string song_from_start_to_end;
+    inline std::string sing(int start = 0, int end = 0) {
+        std::string song_from_start_to_end = "";
 
+        for(int i = start; i >= end; i--) {
+            if(i < start) song_from_start_to_end += "\n";
+            if(i == 0) {
+                song_from_start_to_end += last;
+            } else if(i == 1) {
+                song_from_start_to_end += verse_1;
+            } else {
+                song_from_start_to_end += std::to_string(i) + " bottles of beer on the wall, " + std::to_string(i) + " bottles of beer.\nTake one down and pass it around, " + std::to_string(i-1) + " bottles of beer on the wall.\n";
+            }
+        }
 
         return song_from_start_to_end;
     }
-
 
 }
 
--- exercism/to_solve/beersong/main.cpp
+++ exercism/to_solve/beersong/main.cpp
@@ -1,14 +1,27 @@
 #include "beer_song.h"
-#include <ifstream>
+#include <iostream>
+#include <fstream>
 #include <string>
 
-//TODO
-//add file stream for test
+using std::getline; using std::ifstream;
+using std::cout; using std::endl;
+using std::string; using std::stoi;
+
 
 int main() {
+    ifstream input("input.in");
 
-    
-
+    if(input.is_open()) {
+        string res, line;
+        while(getline(input, line)) {
+            if(line[0] == 'v')
+                cout << beer_song::verse(int(line[2]-48));
+            else if(line[0] == 's')
+                cout << beer_song::sing(int(line[2]-48), int(line[4]-48));
+        }
+    } else { 
+        cout << "input.in not found." << endl;
+    }
 
     return 0;
 }
