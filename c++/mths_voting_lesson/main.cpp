153cbfdb1968acc58568c7012749d7243e01cdda
ClintNunez <clintnunez14@gmail.com>
2022-12-05 14:59:12 +0800

    progress in adding isbn upc

--- c++/mths_voting_lesson/main.cpp
+++ c++/mths_voting_lesson/main.cpp
@@ -15,9 +15,8 @@ using std::map;
 using std::setw; using std::setfill;
 
 // CURRENT TODO
-// TODO add functions for all methods of voting
-// TODO add function in getting file input that allows "1 2 3 4" inputs instead of separated lines
-// TODO implement try catch
+// TODO add ISBN
+// TODO add UPC
  
 // FINISHED TODO
 // TODO add test in Get_Data_From_User and Get_Data_From_File to check if candidates are >= 2. less than 2 candidates are not allowed
@@ -28,6 +27,9 @@ using std::setw; using std::setfill;
 //
 
 const string CLS = "\033[2J\033[1;1H";
+const string VOTING_FILE = "voting.txt";
+const string ISBN_FILE = "isbn.txt";
+const string UPC_FILE = "upc.txt";
 
 vector<pair<string, vector<int>>> candidates;
 vector<int> votes_per_group;
@@ -41,6 +43,7 @@ vector<int> rankings;
 
 // for printing Data
 int longest_name_len = -1, longest_num_len = -1;
+//----------------Inputs------------------
 void Get_User_Input() {
     cout << "Number of candidates: ";
     cin >> candidates_num;
@@ -99,9 +102,10 @@ void Get_User_Input() {
     }
 }
 
-void Get_File_Input() {
+void Get_Voting_File_Input() {
     string line;
-    ifstream inputFile("inputFile.in");
+    ifstream inputFile(VOTING_FILE);
+    // TODO learn how to close ifstream
     if(inputFile.is_open()){
         getline(inputFile, line);
         candidates_num = std::stoi(line);
@@ -133,8 +137,59 @@ void Get_File_Input() {
 
 }
 
-// used by most of the methods inorder to iterate through the rankings of each candidate
-vector<int> each_candidate_rank;
+vector<string> isbn_check;
+vector<string> isbn_find;
+vector<string> upc_check;
+vector<string> upc_find;
+
+void Get_IBSN_UPC_File_Input(string file_name) {
+    string line;
+    int input_ctr;
+    if(file_name == "isbn.txt") {
+        ifstream isbn_input(file_name);
+        while(getline(isbn_input, line)) {
+            if(line == "c") {
+                getline(isbn_input, line);
+                input_ctr = std::stoi(line);
+                for(int i = 0; i < input_ctr; i++) {
+                    getline(isbn_check, line);
+                    isbn_check.push_back(line);
+                }
+            }else if(line == "f") {
+                getline(isbn_input, line);
+                input_ctr = std::stoi(line);
+                for(int i = 0; i < input_ctr; i++) {
+                    getline(isbn_check, line);
+                    isbn_find.push_back(line);
+                }
+            }
+        }
+    } else if(file_name == "upc.txt") {
+        ifstream upc_input(file_name);
+        while(getline(upc_input, line)) {
+            if(line == "c") {
+                getline(upc_input, line);
+                input_ctr = std::stoi(line);
+                for(int i = 0; i < input_ctr; i++) {
+                    getline(upc_input, line);
+                    upc_check.push_back(line);
+                }
+            }else if(line == "f") {
+                getline(upc_input, line);
+                input_ctr = std::stoi(line);
+                for(int i = 0; i < input_ctr; i++) {
+                    getline(upc_input, line);
+                    upc_find.push_back(line);
+                }
+            }
+        }
+    }
+}
+
+//---------------------------------------------
+
+//----------------Voting Methods------------------
+// used by most of the methods inorder to iterate through the rankings of each candidate vector<int> each_candidate_rank;
 
 vector<pair<string,vector<int>>> plurality_result;
 void Plurality_Method() {
@@ -304,98 +359,320 @@ void Print_Data_Table() {
     cout << "\n\n";
 
 }
+//---------------------------------------------
 
-void Opening_Prompt(){
-    cout << "Code for Voting and Apportionment\n" << endl;
-    cout << "Choose input format:" << endl;
-    cout << "1) User Input" << endl;
-    cout << "2) File Input(Make sure to name the file \"inputFile.in\" and have it placed in the same folder as this cpp file)" << endl;
+//----------------ISBN Methods------------------
+
+
+
+
+//----------------UPC Methods------------------
+
+//---------------------------------------------
+void Main_Menu() {
+    cout << "Choose Topic:\n" << endl;
+    cout << "1) Voting" << endl;
+    cout << "2) ISBN" << endl;
+    cout << "3) UPC" << endl;
     cout << "0) Exit" << endl;
     cout << "> ";
 }
 
-void Methods_Prompt(){
+void Input_Method(string file_name) {
+    cout << "Choose input format:" << endl;
+    cout << "1) User Input" << endl;
+    cout << "2) File Input(Make sure to name the file \"" << file_name << "\" and have it placed in the same folder as this cpp file)" << endl;
+    cout << "0) Back" << endl;
+    cout << "-1) Exit" << endl;
+    cout << "> ";
+}
+
+void Voting_Methods_Prompt() {
     cout << "Choose method to use:" << endl;
     cout << "1) Plurality Method" << endl;
     cout << "2) Borda Count Method" << endl;
     cout << "3) Pairwise Method" << endl;
-    cout << "0) Exit" << endl;
+    cout << "0) Back" << endl;
+    cout << "-1) Exit"
+    cout << "> ";
+}
+
+void ISBN_UPC_Method_Prompt() {
+    cout << "Choose method to use:" << endl;
+    cout << "1) Check Validity" << endl;
+    cout << "2) Find Check Digit" << endl;
+    cout << "0) Back" << endl;
+    cout << "-1) Exit"
     cout << "> ";
 }
 
 int main() {
     int choice;
     char continue_choice;
+    bool back, valid_input;
 
     cout << CLS;
 
-    Opening_Prompt(); 
-    while(true) { 
+    do { 
+        Main_Menu(); 
+        back = false;
+        valid_input = false;
         cin >> choice;
 
         if(choice == 0) {
             cout << "Exiting" << endl;
             return 0;
         } else if(choice == 1) {
-            Get_User_Input();
-            break;
+            // Voting topic
+
+            // Get input
+            do {
+                Input_Method(VOTING_FILE);
+                
+                while(true) {
+                    cin >> choice; 
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        valid_input = true;
+                        break;
+                    } else if(choice == 1) {
+                        Get_User_Input();
+                        valid_input = true;
+                        break;
+                    } else if(choice == 2) {
+                        Get_File_Input(VOTING_FILE);
+                        valid_input = true;
+                        break;
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                        break;
+                    }
+                }
+            } while(!valid_input);
+
+            valid_input = false;
+
+            // Choose Methods
+            while(!back) { // !back so that when the user wants to go back, the code will not run this.
+                Voting_Methods_Prompt();
+
+                while(true) {
+                    cin >> choice;
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        break;
+                    } else if(choice == 1) {
+                        if(plurality_result.empty()) {
+                            Plurality_Method();
+                        }
+                        Print_Plurality_Result();
+                        break;
+                    } else if(choice == 2) {
+                        if(borda_result.empty()) {
+                            Borda_Method();
+                        }
+                        Print_Borda_Result();
+                        break; 
+                    } else if(choice == 3) {
+                        if(pairwise_result.empty()) {
+                            Pairwise_Method();
+                        }
+                        Print_Pairwise_Result();
+                        break; 
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                    }
+                }
+                
+                cout << "Continue[y/n]?\n> ";
+                cin >> continue_choice;
+
+                tolower(continue_choice);
+                while(continue_choice != 'y' && continue_choice != 'n') {
+                    cout << "Please enter y/Y or n/N only.\n> ";
+                    cin >> continue_choice;
+                }
+
+                if(continue_choice == 'y') {
+                    cout << CLS;
+                } else {
+                    break; // will go back to the main menu
+                } 
+            }
+
         } else if(choice == 2) {
-            Get_File_Input();
-            break; 
+            // ISBN
+
+            // Get input
+            do {
+                Input_Method(ISBN_FILE);
+
+                while(true) {
+                    cin >> choice;
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        valid_input = true;
+                        break;
+                    } else if(choice == 1) {
+                        Get_User_Input();
+                        valid_input = true;
+                        break;
+                    } else if(choice == 2) {
+                        Get_File_Input(ISBN_FILE);
+                        valid_input = true;
+                        break;
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                        break;
+                    }
+                }
+            } while(!valid_input);
+
+            valid_input = false;
+            
+            // Execute methods
+            while(!back) {
+                ISBN_UPC_Method_Prompt();
+
+                while(true) {
+                    cin >> choice;
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        break;
+                    } else if(choice == 1) {
+                        // insert method for validity checking
+                        break;
+                    } else if(choice == 2) {
+                        // insert method for finding check digit 
+                        break; 
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                    }
+                }
+                
+                cout << "Continue[y/n]?\n> ";
+                cin >> continue_choice;
+
+                tolower(continue_choice);
+                while(continue_choice != 'y' && continue_choice != 'n') {
+                    cout << "Please enter y/Y or n/N only.\n> ";
+                    cin >> continue_choice;
+                }
+
+                if(continue_choice == 'y') {
+                    cout << CLS;
+                } else {
+                    break; // will go back to the main menu
+                } 
+
+            }
+
+        } else if(choice == 3) {
+            // UPC
+
+            // Get input
+            do {
+                Input_Method(UPC_FILE);
+
+                while(true) {
+                    cin >> choice;
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        valid_input = true;
+                        break;
+                    } else if(choice == 1) {
+                        Get_User_Input();
+                        valid_input = true;
+                        break;
+                    } else if(choice == 2) {
+                        Get_File_Input(UPC_FILE);
+                        valid_input = true;
+                        break;
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                        break;
+                    }
+                }
+            } while(!valid_input);
+
+            valid_input = false;
+            
+            // Execute methods
+            while(!back) {
+                ISBN_UPC_Method_Prompt();
+
+                while(true) {
+                    cin >> choice;
+
+                    if(choice == -1) {
+                        cout << "Exiting" << endl;
+                        return 0;
+                    } else if(choice == 0) {
+                        back = true;
+                        break;
+                    } else if(choice == 1) {
+                        // insert method for validity checking
+                        break;
+                    } else if(choice == 2) {
+                        // insert method for finding check digit 
+                        break; 
+                    } else {
+                        std::cout << "input number that is within the choices.\n> "; 
+                        std::cin.clear();
+                        std::cin.ignore(10000, '\n');
+                    }
+                }
+                
+                cout << "Continue[y/n]?\n> ";
+                cin >> continue_choice;
+
+                tolower(continue_choice);
+                while(continue_choice != 'y' && continue_choice != 'n') {
+                    cout << "Please enter y/Y or n/N only.\n> ";
+                    cin >> continue_choice;
+                }
+
+                if(continue_choice == 'y') {
+                    cout << CLS;
+                } else {
+                    break; // will go back to the main menu
+                } 
+
+            }
         } else {
             std::cout << "input number that is within the choices.\n> "; 
             std::cin.clear();
             std::cin.ignore(10000, '\n');
-        }
-    }
-    cout << CLS;
-
-    do {
-        Print_Data_Table();
-        Methods_Prompt();
-
-        while(true) {
-            cin >> choice;
-
-            if(choice == 0) {
-                cout << "Exiting" << endl;
-                return 0;
-            } else if(choice == 1) {
-                if(plurality_result.empty()) {
-                    Plurality_Method();
-                }
-                Print_Plurality_Result();
-                break;
-            } else if(choice == 2) {
-                if(borda_result.empty()) {
-                    Borda_Method();
-                }
-                Print_Borda_Result();
-                break; 
-            } else if(choice == 3) {
-                if(pairwise_result.empty()) {
-                    Pairwise_Method();
-                }
-                Print_Pairwise_Result();
-                break; 
-            } else {
-                std::cout << "input number that is within the choices.\n> "; 
-                std::cin.clear();
-                std::cin.ignore(10000, '\n');
-            }
-        }
-
-        cout << "Continue[y/n]?\n> ";
-        cin >> continue_choice;
-
-        tolower(continue_choice);
-        while(continue_choice != 'y' && continue_choice != 'n') {
-            cout << "Please enter y/Y or n/N only.\n> ";
-            cin >> continue_choice;
-        }
-
-        if(continue_choice == 'y') {
-            cout << CLS;
         }
 
     } while(continue_choice != 'n');
