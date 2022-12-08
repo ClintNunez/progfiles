02f52773bd656432143d359b485bb77f78516d76
ClintNunez <clintnunez14@gmail.com>
2022-12-08 20:51:44 +0800

    finished cryptosquare

--- exercism/to_solve/cryptosquare/crypto_square.h
+++ exercism/to_solve/cryptosquare/crypto_square.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <vector>
 
-// TODO try using references
-
 namespace crypto_square {
     class cipher {
         private:
@@ -15,34 +13,23 @@ namespace crypto_square {
             std::vector<std::string> segments_result;
 
         public:
-            cipher(std::string input_word) {
+            cipher(std::string &input_word) {
                 word = input_word;
             }
-
-            // methods
 
             // will remove space, special characters and 
             // all letters are lowercased
             std::string normalize_plain_text() {
                 result = "";
                 for(int i = 0; i < int(word.length()); i++) {
-                    if(isalpha(word[i])) {
-                        if(isupper(word[i]))
-                            result.push_back(char(word[i] + 32));
-                        else 
-                            result.push_back(word[i]);
-                    } else if(isdigit(word[i])) {
-                        result.push_back(word[i]);
-                    }
-
+                    if(isalnum(word[i]))
+                        result.push_back(tolower(word[i]));
                 }
 
                 return result;
             }
 
             // will segment the word into sub strings
-            // same logic for the number of column and row
-            // for the cipher
             std::vector<std::string> plain_text_segments() {
                 std::string normalized = crypto_square::cipher::normalize_plain_text();
                 
@@ -65,7 +52,7 @@ namespace crypto_square {
                 int columns = ceil(sqrt(len));
                 int index;
                 result = "";
-                
+
                 for(int i = 0; i < columns; i++) {
                     for(int j = 0; j < columns; j++) {
                         index = columns * j + i;
@@ -82,25 +69,33 @@ namespace crypto_square {
             // depending on the column and row
             std::string normalized_cipher_text() {
                 std::string normalized = crypto_square::cipher::normalize_plain_text();
+
+                if(int(normalized.length()) == 0) {
+                    return normalized;
+                }
+                
                 int len = normalized.length();
                 int columns = ceil(sqrt(len));
+                int rows = ceil(double(len) / double(columns));
                 int index;
                 result = "";
 
+                for(int i = 0; i < ((columns * rows) - len); i++)
+                    normalized.push_back(' ');
 
+                len = normalized.length();
 
                 for(int i = 0; i < columns; i++) {
                     for(int j = 0; j < columns; j++) {
                         index = columns * j + i;
-
                         if(index < len)
                             result.push_back(normalized[index]);
-                        else 
+                        else if(j > rows)
                             result.push_back(' ');
                     }
-                    result.push_back(' ');
+                    if(i != columns - 1) 
+                        result.push_back(' ');
                 }
-                
 
                 return result;
             }
--- exercism/to_solve/cryptosquare/main.cpp
+++ exercism/to_solve/cryptosquare/main.cpp
@@ -2,13 +2,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using std::cout; using std::endl;
-using std::string;
+using std::string; using std::vector;
 using std::ifstream; using std::getline;
 
-
 int main() {
+    ifstream input("input.in");
+    string line;
+
+    getline(input, line);
+
+    crypto_square::cipher Cipher(line);
+
+    string normalize_plain_text_res = Cipher.normalize_plain_text();
+    vector<string> plain_text_segments_res = Cipher.plain_text_segments();
+    string cipher_text_res = Cipher.cipher_text();
+    string normalized_cipher_text_res = Cipher.normalized_cipher_text();
+
+    cout << "normalize plain text: " << normalize_plain_text_res << endl;
+
+    cout << "plain text segments: ";
+    for(auto it = plain_text_segments_res.begin(); it != plain_text_segments_res.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    
+    cout << "cipher text: " << cipher_text_res << endl;
+
+    cout << "normalized cipher text: " << normalized_cipher_text_res << endl;
+
 
     return 0;
 }
