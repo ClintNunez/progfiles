ec94540c79141e9c501af834904023defa4b2133
ClintNunez <clintnunez14@gmail.com>
2022-12-09 22:53:14 +0800

    attempt in phonenumber

--- exercism/to_solve/phonenumber/main.cpp
+++ exercism/to_solve/phonenumber/main.cpp
@@ -3,6 +3,10 @@
 #include <stdexcept>
 #include <string>
 
+using std::cout; using std::endl;
+using std::string; using std::getline;
+using std::ifstream;
+
 int main() {
 
     return 0;
--- exercism/to_solve/phonenumber/phone_number.h
+++ exercism/to_solve/phonenumber/phone_number.h
@@ -3,46 +3,60 @@
 
 #include <string>
 #include <stdexcept>
-
-/*
-NANP numbers are ten-digit numbers consisting of a three-digit Numbering Plan Area code, 
-commonly known as area code, followed by a seven-digit local number. The first three digits 
-of the local number represent the exchange code, followed by the unique four-digit number 
-which is the subscriber number.
-
-The format is usually represented as
-
-(NXX)-NXX-XXXX
-where N is any digit from 2 through 9 and X is any digit from 0 through 9.
-
-Your task is to clean up differently formatted telephone numbers by removing punctuation and 
-the country code (1) if present.
-
-For example, the inputs
-
-+1 (613)-995-0253
-613-995-0253
-1 613 995 0253
-613.995.0253
-should all produce the output
-
-6139950253
-Sample test case that idk how to do
-    REQUIRE("223" == phone_number::phone_number("+1 (223) 456-7890").area_code());
-
-    const phone_number::phone_number phone("+1 (223) 456-7890");
-    REQUIRE("(223) 456-7890" == std::string(phone));
-*/
-
-/* throws domain error when digits is <= 9 or >= 11
- *
- * */
-
+#include <sstream>
 
 namespace phone_number {
-    class phone_number {
 
+    class phone_number {
+        private:
+            std::string extracted_digits;
+            char country_code;
+            bool check_validity();
+        public:
+            phone_number(std::string& input_number) {
+                for(char c : input_number) {
+                    if(std::isdigit[c]) 
+                        extracted_digits.push_back(c);
+                }
+
+                if(extracted_digits.size() == 11) {
+                    country_code = extracted_digits[0];
+                    extracted_digits.erase(begin(extracted_digits));
+                }
+
+                if(!check_validity()) 
+                    throw std::domain_error("");
+            }
+
+            bool check_validity() {
+                return extracted_digits.size() == 10
+                    && std::stoi(area_code()) >= 200
+                    && std::stoi(exchange_code()) >= 200
+                    && country_code == '1';
+            }
+            
+            std::string number() {
+                return extracted_digits;
+            }
+
+            std::string area_code() {
+                return extracted_digits.substr(0, 3);
+            }
+
+            std::string exchange_code() {
+                return extracted_digits.substr(3,3);
+            }
+
+            std::string subscriber_num() {
+                return extracted_digits.substr(6,4);
+            }
+
+            phone_number::operator std::string() {
+                std::stringstream output{""};
+                output << '(' << area_code() << ') ' << exchange_code() << '-' << subscriber_num();
+                return output.str();
+            }
     };
 }
 
-#endif
+#endif 
