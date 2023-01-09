8db34ac719b264bf0397fa84699113c80e136801
ClintNunez <clintnunez14@gmail.com>
2023-01-09 18:43:34 +0800

    finished the code, add comments or some optimization

--- c++/Spiral_Of_Prime/main.cpp
+++ c++/Spiral_Of_Prime/main.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 
 void create_list_of_primes(int &rDimension, vector<bool> &rPrimes) { // Time Complexity: O(n*log(log(n)))
-    for (int p = 2; p * p <= rDimension; p++) {
+    for (int p = 2; p * p <= rDimension * rDimension; p++) {
         if (rPrimes.at(p)) {
-            for (int i = p * p; i <= rDimension; i += p) {
+            for (int i = p * p; i <= rDimension * rDimension; i += p) {
                 rPrimes.at(i) = false;
             }
         }
@@ -27,118 +27,113 @@ void create_list_of_primes(int &rDimension, vector<bool> &rPrimes) { // Time Com
 // create spiral
 void createSpiral(int &rDimension, vector<bool> &rPrimes ,vector<vector<char>> &rMatrix) {
     const char PATH_LEFT = '<', PATH_RIGHT = '>', PATH_UP = '^', 
-          PATH_DOWN = 'v', NOT_PRIME = 'o', PRIME = 'O';
+          PATH_DOWN = 'v', NOT_PRIME = 'o', PRIME = '@';
 
-    int x, y, direction = 0, orig_dimension = rDimension / 3, overall_cells = pow((rDimension / 3), 2),
+    int x = 1, y = 1, direction = 0, orig_dimension = rDimension / 3, overall_cells = pow((rDimension / 3), 2),
         step = 0, number_of_steps = 1, repeated_steps = 0;
     
     // finds the center of the 2d vector by checking if the dimension is even or odd
     // if even, the center is x = (dimension / 2) - 2 and y = (dimension / 2) + 1
     // if odd, the center is x = dimension / 2 and y = dimension / 2
     if(rDimension % 2 == 0) {
-        x = (rDimension / 2) - 2; y = (rDimension / 2) + 1;
+        x += (rDimension / 2) - 3; y += (rDimension / 2);
     } else {
-        x = (rDimension / 2); y = (rDimension / 2);
+        x += (rDimension / 2); y += (rDimension / 2);
     }
 
     // loops for the number of cells in the original dimension(dimension given by user without multiplying by 3) 
     for(int i = 1; i <= overall_cells; i++) {
 
         if(i == 1) {
-            rMatrix[x][y] = NOT_PRIME;
-            y++;
-            rMatrix[x][y] = PATH_RIGHT;
-            y++;
+            rMatrix[y][x] = NOT_PRIME;
+            x++;
+            rMatrix[y][x] = PATH_RIGHT;
+            x++;
 
-            step++;
-        } if(i == overall_cells) {
-
+        } else if(i == overall_cells) {
             switch(direction) {
                 case 0:
-                    rMatrix[x][y] = PATH_RIGHT;
-                    y++;
+                    rMatrix[y][x] = PATH_RIGHT;
+                    x++;
                     break;
                 case 1:
-                    rMatrix[x][y] = PATH_UP;
-                    x--;
-                    break;
-                case 2:
-                    rMatrix[x][y] = PATH_LEFT;
+                    rMatrix[y][x] = PATH_UP;
                     y--;
                     break;
+                case 2:
+                    rMatrix[y][x] = PATH_LEFT;
+                    x--;
+                    break;
                 case 3:
-                    rMatrix[x][y] = PATH_DOWN;
-                    x++;
+                    rMatrix[y][x] = PATH_DOWN;
+                    y++;
                     break;
             }
 
-            if(rPrimes[i - 2])
-                rMatrix[x][y] = PRIME;
+            if(rPrimes[i])
+                rMatrix[y][x] = PRIME;
             else 
-                rMatrix[x][y] = NOT_PRIME;
+                rMatrix[y][x] = NOT_PRIME;
 
         } else {
             for(int j = 0; j < 3; j++) {
                 if(j == 1) {
-                    if(rPrimes[i - 2])
-                        rMatrix[x][y] = PRIME;
+
+                    if(rPrimes[i])
+                        rMatrix[y][x] = PRIME;
                     else 
-                        rMatrix[x][y] = NOT_PRIME;
+                        rMatrix[y][x] = NOT_PRIME;
+
+                    // change directions
+                    step++;
+                    if(step == number_of_steps) {
+                        repeated_steps++;
+                        direction = (direction == 3) ? 0 : direction + 1;
+                        step = 0;
+                    }
+
+                    if(number_of_steps != orig_dimension - 1) {
+                        if(repeated_steps == 2) {
+                            repeated_steps = 0;
+                            number_of_steps++;
+                        }
+                    }
 
                     // move current index
                     switch(direction) {
                         case 0:
-                            y++; // right
+                            x++; // right
                             break;
                         case 1:
-                            x--; // up
+                            y--; // up
                             break;
                         case 2:
-                            y--; // left
+                            x--; // left
                             break;
                         case 3:
-                            x++; // down
+                            y++; // down
                             break;
                     }
                     
-                    // change directions
-                    if(step == number_of_steps) {
-                        direction = (direction == 3) ? 0 : direction + 1;
-                    }
                 } else {
                     switch(direction) {
                         case 0:
-                            rMatrix[x][y] = PATH_RIGHT;
-                            y++;
-                            break;
-                        case 1:
-                            rMatrix[x][y] = PATH_UP;
-                            x--;
-                            break;
-                        case 2:
-                            rMatrix[x][y] = PATH_LEFT;
-                            y--;
-                            break;
-                        case 3:
-                            rMatrix[x][y] = PATH_DOWN;
+                            rMatrix[y][x] = PATH_RIGHT;
                             x++;
                             break;
+                        case 1:
+                            rMatrix[y][x] = PATH_UP;
+                            y--;
+                            break;
+                        case 2:
+                            rMatrix[y][x] = PATH_LEFT;
+                            x--;
+                            break;
+                        case 3:
+                            rMatrix[y][x] = PATH_DOWN;
+                            y++;
+                            break;
                     }
-                }
-            }
-
-            step++;
-        }
-
-
-        if(number_of_steps != orig_dimension - 1) {
-            if(step == number_of_steps) {
-                step = 0;
-                repeated_steps++;
-
-                if(repeated_steps == 2) {
-                    repeated_steps = 0;
-                    number_of_steps++;
                 }
             }
         }
@@ -158,11 +153,15 @@ void printSpiral(vector<vector<char>> &rMatrix) {
 int main() {
     cout << "Enter dimensions of rMatrix: ";
     int dimension; cin >> dimension;
-    dimension *= 3;
 
-    vector<bool> primes(dimension + 1, true); // +1 for good measure, remove after testing
+    vector<bool> primes(dimension * dimension + 1, true); 
     create_list_of_primes(dimension, primes);
 
+    // multiply the dimension by 3 to make room for the paths
+    // _ _ _
+    // _ @ <
+    // _ v _
+    dimension *= 3;
     // Initialize a 2d vector and set its elements to ' '
     vector<vector<char>> rMatrix(dimension, vector<char>(dimension, ' '));
 
