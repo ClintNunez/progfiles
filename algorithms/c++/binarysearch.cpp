31f3ee893711735513d3b802e0ee52c929dec92b
ClintNunez <clintnunez14@gmail.com>
2023-01-15 15:55:18 +0800

    added my own def of binary search and fixed some comments

--- algorithms/c++/binarysearch.cpp
+++ algorithms/c++/binarysearch.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 #include <vector>
 
+// Binary search divides the list of elements until it finds the data that it is looking for
+// Meaning that each iteration, the size of the list gets smaller and smaller until there is only one element
+// on the list. If the remaining element is equal to the data being searched then it is on the list, else it
+// is not on the list.
 int Binary_Search(std::vector<int> data, int &num_to_find) {
-    int first = 0, last = data.size() - 1, midpoint; // 1
+    int first = 0, last = data.size() - 1, midpoint; // 3
     
     while(first <= last) { // log sub 2 n
         // get the midpoint of the current sub list(array or vector) 
@@ -14,14 +18,12 @@ int Binary_Search(std::vector<int> data, int &num_to_find) {
             // returns index of the same element if found
             return midpoint; // 1
         else if(data.at(midpoint) < num_to_find) // 1
-            // moves the position of first to midpoint + 1 which removes all of the
-            // data to the LEFT of the current element and the current element itself
-            // from the process.
+            // moves the position of first to midpoint + 1 which removes all the current
+            // element including all the elements to it's left
             first = midpoint + 1; // 1
         else  // 1
-            // moves the position of last to midpoint - 1 which removes all of the
-            // data to the RIGHT of the current element and the current element itself
-            // from the process.
+            // moves the position of last to midpoint - 1 which removes all the current
+            // element including all the elements to it's right
             last = midpoint - 1; // 1
     }
     
@@ -29,7 +31,7 @@ int Binary_Search(std::vector<int> data, int &num_to_find) {
     return -1; // 1
 }
 // Computation: 
-// time complexity = 1 + log sub 2 n + 1 + 1 + 1 + 1 + 1 + 1 + 1 = log sub 2 + 8, O(log n)
+// time complexity = 3 + log sub 2 n + 1 + 1 + 1 + 1 + 1 + 1 + 1 = log sub 2 + 8, O(log n) Logarithmic time
 
 int main() {
     std::vector<int> data = { 1, 2, 3, 4, 5, 6 };
