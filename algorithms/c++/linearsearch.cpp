c116a3e5db052a11cbc4f5ab2aa0fff806e15191
ClintNunez <clintnunez14@gmail.com>
2023-01-15 15:11:13 +0800

    fixed comments and time complexity computation

--- algorithms/c++/linearsearch.cpp
+++ algorithms/c++/linearsearch.cpp
@@ -3,18 +3,22 @@
 
 int linear_search(std::vector<int> data, int &element_to_find) {
 
-    // loops n number of times where n is the size of data.
+    // loops n number of times, where n is the size of data.
+    // and compares each element of data to element_to_find.
+    // if the current element is equal to element_to_find then 
+    // return the current index.
+    // else return -1 in this case
     for(int i = 0; i < data.size(); i++) { // n
-        if(data.at(i) == element_to_find) // n
+        if(data.at(i) == element_to_find) // 1
             // returns the index where data.at(i) is equal to element_to_find
             return i; // 1
     }
 
-    // returns -1 if not found
     return -1; // 1
 }
 // Computation:
-// time complexity = n + n + 1 + 1 = 2n + 2, O(n)
+// time complexity = n + 1 + 1 + 1 = n + 3, O(n) Linear time
+//                   or n + 1 + 1 = n + 2
 
 int main() {
     std::vector<int> data = { 1, 2, 3, 4, 5, 10 };
