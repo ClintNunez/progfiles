da4e300a5d73e9b9e5be77d631c60b10ea210699
ClintNunez <clintnunez14@gmail.com>
2023-01-16 21:44:49 +0800

    finished code

--- algorithms/c++/recursive_binarysearch.cpp
+++ algorithms/c++/recursive_binarysearch.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include <vector>
 
-// Binary search divides the list of elements until it finds the data that it is looking for
-// Meaning that each iteration, the size of the list gets smaller and smaller until there is only one element
-// on the list. If the remaining element is equal to the data being searched then it is on the list, else it
-// is not on the list.
-
 // Recursive implementation of binary search
-int Recursive_Binary_Search(std::vector<int> data, int num_to_find) {
+// the body of the function run at constant time except for the functoin itself which runs at log sub 2 n
+int Recursive_Binary_Search(std::vector<int> data, int &num_to_find, int &midpoint) { // log sub 2 n
 
-    int midpoint;
     if(data.size() == 0) {
-        return -1;
+        return -1; 
     } else {
-        midpoint = data.size() / 2; 
+        midpoint = data.size() / 2;
 
         if(data.at(midpoint) == num_to_find) {
             return midpoint;
         } else if(data.at(midpoint) < num_to_find){
-            std::cout << "remove first reached" << std::endl;
-            data.erase(data.begin(), data.begin() + midpoint);
-            Recursive_Binary_Search(data, num_to_find);
+            if(data.size() != 1) {
+                data.erase(data.begin(), data.begin() + midpoint);
+            } else {
+                return Recursive_Binary_Search(data, num_to_find, midpoint);
+            }
         } else {
-            std::cout << "remove first reached" << std::endl;
-            data.erase(data.begin() + midpoint, data.end());
-            Recursive_Binary_Search(data, num_to_find);
+            if(data.size() != 1) {
+                data.erase(data.begin() + midpoint, data.end());
+            } else {
+                return Recursive_Binary_Search(data, num_to_find, midpoint);
+            }
         }
     }
-    
-    // returns -1 if not found
-    return -1; // 1
+
+    // will not be reached, here for removing the warning
+    return -2;
 }
 // Computation: 
 // time complexity = 3 + log sub 2 n + 1 + 1 + 1 + 1 + 1 + 1 + 1 = log sub 2 + 8, O(log n) Logarithmic time
@@ -44,8 +43,10 @@ int main() {
 
     std::cout << "Enter num to find: ";
     int num_to_find; std::cin >> num_to_find;
+    int midpoint;
 
-    int result = Recursive_Binary_Search(data, num_to_find);
+    int result = Recursive_Binary_Search(data, num_to_find, midpoint);
+    std::cout << "result: " << result << std::endl;
 
     if(result != -1) 
         std::cout << "Number found at index " << result << std::endl;
