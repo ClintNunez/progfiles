afa60ba8858fdefd737eab83b168ceba4b909f61
ClintNunez <clintnunez14@gmail.com>
2023-01-24 12:02:27 +0800

    added description for bubble sort

--- algorithms/c++/bubblesort.cpp
+++ algorithms/c++/bubblesort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+// check if sorted ascendingly
 bool is_sorted(std::vector<int> &data) {
     for(int i = 0; i < data.size() - 1; i++) {
         if(data.at(i) > data.at(i + 1)) {
@@ -10,6 +11,22 @@ bool is_sorted(std::vector<int> &data) {
     return true;
 }
 
+// Bubblesort checks if the list of data is sorted. If it is not sorted then it will check the first element if it is greater
+// than the next element if you want to sort your data ascendingly. if it is greater than the next element then swap them.
+// Then it will move to the next index and check again if it is greater than the next element. It will do this until the second
+// to the last element. After passing through the list then it will check again if it is sorted already, if not then repeat
+// this process.
+// Ex: data = { 4, 1, 5, 3, 2 }
+// check if sorted
+// iteration 1: 1, 4, 5, 3, 2
+// iteration 2: 1, 4, 5, 3, 2
+// iteration 3: 1, 4, 3, 5, 2
+// iteration 4: 1, 4, 3, 2, 5
+// check if sorted
+// iteration 1: 1, 3, 4, 2, 5
+// iteration 1: 1, 3, 2, 4, 5
+// check if sorted
+// iteration 1: 1, 2, 3, 4, 5
 void bubblesort(std::vector<int> &data) {
     while(!is_sorted(data)) {
         for(int i = 0; i < data.size() - 1; i++) {
