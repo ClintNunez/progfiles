33f90ac3060e0decd3b7a116525c665909380f51
ClintNunez <clintnunez14@gmail.com>
2023-02-01 22:05:37 +0800

    started notes for Number types

--- dart_only/basics.dart
+++ dart_only/basics.dart
@@ -1,16 +1,27 @@
-void printInteger(Object aNumber) {
-    print('The number is $aNumber');
-}
+/*
+### TYPES
+Search for:
+    /T_ + Number || Strings || Booleans || Lists || Sets || Maps || Runes || Symbols
+    
 
-void printString(Object aString) {
-    print('The string is $aString');
-}
+*/
 
 void main() {
-    // Obects can have any types of data as | TODO test for all basic data types
-    Object number = 42;
-    printInteger(number);
-    number = "hello";
-    printInteger(number);
-    printString(number);
+    
+
+
+}
+
+void T_Numbers() {
+    var x = 1;  // int x = 1;
+    var hex = 0x32; // int hex = 0x32;
+
+    var y = 1.1; // double y = 1.1;
+    var exponent = 1.1e5; // double expnonet = 1.1e5;
+
+    double z = 1; // Equivalent to double z = 1.0; "Integer literals are automatically converted to doubles when necessary" TODO test for arithmetic operations
+
+    // variables declared as num can have both integer and double values
+    num a = 1; 
+    a += 1.1e5;
 }
