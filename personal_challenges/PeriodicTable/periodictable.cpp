525565997f2349b21091471f4bbb5d91555b49c8
ClintNunez <clintnunez14@gmail.com>
2023-02-03 20:42:18 +0800

    initial commit

---
+++ personal_challenges/PeriodicTable/periodictable.cpp
@@ -0,0 +1,13 @@
+/*
+    library for dealing with json files:
+    https://github.com/nlohmann/json
+    JSON file from: 
+    https://github.com/Bowserinator/Periodic-Table-JSON 
+*/
+#include <iostream>
+
+
+int main() {
+
+    return 0;
+}
