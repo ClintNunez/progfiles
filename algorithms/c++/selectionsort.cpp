a9b624ebffa8993c638a209bbbd1478f20ffb86c
ClintNunez <clintnunez14@gmail.com>
2023-02-06 11:06:12 +0800

    added desciprtion to selection sort

--- algorithms/c++/selectionsort.cpp
+++ algorithms/c++/selectionsort.cpp
@@ -3,42 +3,50 @@
 #include <vector>
 
 typedef std::vector<int> vectorInt;
-// check if i can access the elements of a pointer to an array using '->'
+typedef std::vector<int> *pVectorInt;
 
 // Selection sort finds the smallest number from the unsorted list and puts it to a new list while also removing that number from the unsorted list. 
-vectorInt Selection_Sort(vectorInt unsorted_Vector, vectorInt sorted_vector) {
-    if(!unsorted_Vector.empty()) {
+pVectorInt Selection_Sort(pVectorInt pUnsorted_Vector, pVectorInt pSorted_Vector) {
+
+    if(!pUnsorted_Vector->empty()) {
         int smallest, index;
 
-        while(!unsorted_Vector.empty()) {
+        while(!pUnsorted_Vector->empty()) {
             // initialize values for comparison
-            smallest = unsorted_Vector.at(0);
+            smallest = pUnsorted_Vector->at(0);
             index = 0;
 
-            for(int i = 1; i < unsorted_Vector.size(); i++) {
-                if(unsorted_Vector.at(i) <= smallest) {
-                    smallest = unsorted_Vector.at(i);
+            // checks elements from index 1 to pUnsorted_Vector->size() - 1 if each of them is less than the element at index 0
+            // if the current element is less than the element at index 0 then replace the value of smallest to the current element
+            // and the value of index to the value of i
+            for(int i = 1; i < pUnsorted_Vector->size(); i++) {
+                if(pUnsorted_Vector->at(i) <= smallest) {
+                    smallest = pUnsorted_Vector->at(i);
                     index = i;
                 }
             }
 
-            unsorted_Vector.erase(unsorted_Vector.begin() + index);
+            // use the value of index to erase the current smallest element in the vector
+            pUnsorted_Vector->erase(pUnsorted_Vector->begin() + index);
 
-            sorted_vector.push_back(smallest);
+            pSorted_Vector->push_back(smallest);
         }
 
     } else {
         std::cout << "Empty" << std::endl;
     }
 
-    return sorted_vector;
+    return pSorted_Vector;
 }
 
 int main() {
     vectorInt unsorted_vector = { 5, 4, 6, 3, 1, 2, 3, 1, 5, 7 };
     vectorInt sorted_vector;
+
+    pVectorInt pUnsorted_Vector = &unsorted_vector;
+    pVectorInt pSorted_Vector = &sorted_vector;
     
-    sorted_vector = Selection_Sort(unsorted_vector, sorted_vector);
+    pSorted_Vector = Selection_Sort(pUnsorted_Vector, pSorted_Vector);
 
     for(int i = 0; i < sorted_vector.size(); i++) {
         // find other ways
