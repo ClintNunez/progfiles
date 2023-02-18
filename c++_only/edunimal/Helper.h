b33d89710fd29aa047217315eea0434e94ebe10c
ClintNunez <clintnunez14@gmail.com>
2023-02-18 17:57:10 +0800

    added Ignore()

--- c++_only/edunimal/Helper.h
+++ c++_only/edunimal/Helper.h
@@ -7,13 +7,17 @@
 /*
  * TODO
  * - Add functions for finding definitions from a binary file.
- *
+ * - Add function for saving names for the animals
  *
  * */
 
 namespace helper {
     inline void CLS() {
         std::cout << "\033[2J\033[1;1H";
+    }
+
+    inline void Ignore() {
+        std::cin.ignore(1);
     }
 }
 
