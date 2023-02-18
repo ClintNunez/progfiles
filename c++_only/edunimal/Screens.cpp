cdb96765cbff1905d2cdc9ceb4fee28a441840e4
ClintNunez <clintnunez14@gmail.com>
2023-02-18 16:47:51 +0800

    Moved the info screen here

--- c++_only/edunimal/Screens.cpp
+++ c++_only/edunimal/Screens.cpp
@@ -1,4 +1,5 @@
 #include "Screens.hpp"
+#include "EduAnimal.hpp"
 #include "Helper.h"
 #include <cctype>
 #include <iostream>
@@ -7,14 +8,15 @@
 /*
  * TODO:
  * - add filtering for inputs, since getline is used, check for: length and extract the character at index 0;
+ * - SplashScreen Should use delay and not ignore()
  * */
 
 Screens::Screens() {}
 
 void Screens::SplashScreen() {
+    helper::CLS();
     std::cout << "House" << std::endl;
     std::cin.ignore();
-    helper::CLS();
 }
 
 void Screens::HomeScreen() {
@@ -22,15 +24,52 @@ void Screens::HomeScreen() {
     // EDUNIMALS TITLE
     //  DOG CAT BIRD  Exit
     //   a   b   c      e 
-    std::cout << "Edunimals hehe" << std::endl;
-    std::cout << "Choose your EduAnimal:\n(a)EduDog - (Canines)\n(b)EduCat - (Felines)\n(c)EduBird - (bird?)\n(e)Exit\n>";
-    std::getline(std::cin, animal_choice);
+    do {
+        helper::CLS();
+        std::cout << "Edunimals hehe" << std::endl;
+        std::cout << "Choose your EduAnimal:\n(1)EduDog - (Canines)\n(2)EduCat - (Felines)\n(3)EduBird - (bird?)\n(4)Exit\n>";
+        std::getline(std::cin, animal_choice);
 
-    animal_choice = tolower(animal_choice[0]);
+        if(animal_choice.length() > 1 || animal_choice[0] < '1' || animal_choice[0] > '4') {
+            std::cout << "Choice is not within the given options. Press [Enter] to redo." << std::endl;
+            std::cin.ignore();
+        } else {
+            break;
+        }
+
+    } while(true);
 }
 
 char Screens::Get_Animal_Choice() {
     return animal_choice[0];
+}
+
+void Screens::DefinitionScreen(std::string search_Term) {
+    // this will display the definition and will use the file searching function from the helper class
+    helper::CLS();
+    std::cout << search_Term << std::endl;
+}
+
+void Screens::InfoScreen(char animal_ID) {
+    helper::CLS();
+    // Show table
+    std::cout << "Table" << std::endl;
+
+    switch(animal_ID) {
+        case '1':
+            EduAnimals.eduDog.Display();
+            EduAnimals.eduDog.Greetings();
+            break;
+        case '2':
+            EduAnimals.eduCat.Display();
+            EduAnimals.eduCat.Greetings();
+            break;
+        case '3':
+            EduAnimals.eduBird.Display();
+            EduAnimals.eduBird.Greetings();
+            break;
+    }
+
 }
 
 void Screens::ContinuePrompt() {
