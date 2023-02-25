238df074783a60bff36e8d0fe3a246ce1247645c
ClintNunez <clintnunez14@gmail.com>
2023-02-25 22:47:34 +0800

    added a test file

--- c++_only/JSON_Reader/main.cpp
+++ c++_only/JSON_Reader/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include "json.hpp"
 
 using json = nlohmann::json;
 
 int main() {
-    
+    std::fstream f("test.json");
+    json data = json::parse(f);    
+           
+    std::cout << data["name"] << std::endl;
 
+    std::cout << data["age"] << std::endl;
 
+    data["name"] = "patrick";
 
+    std::cout << data["name"] << std::endl;
 
     return 0;
 }
