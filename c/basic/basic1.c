2783405d9e247bb9420ff213b8739bd7aa8381cc
ClintNunez <clintnunez14@gmail.com>
2024-02-01 20:35:25 +0800

    added c folder

---
+++ c/basic/basic1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Clint Patrick M. Nunez";
+    char birthday[] = "July 14, 1975";
+    int number = 9999;
+
+    printf("Name: %s\n", name);
+    printf("DOB: %s\n", birthday);
+    printf("Mobile: %i\n", number);
+
+    return 0;
+}
