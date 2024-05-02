57b9244fe13c1ff40099877e942ef708ed1f3e57
ClintNunez <clintnunez14@gmail.com>
2024-05-02 12:49:24 +0800

    minor changes

--- data_structures/c/linkedlist/singly_linked_main.c
+++ data_structures/c/linkedlist/singly_linked_main.c
@@ -7,27 +7,24 @@
 int main() {
     struct sll_node *head = NULL; 
 
-    insert_At_Head(&head, 1);
+    insert_At_Head(&head, 5);
+    insert_At_Head(&head, 4);
+    insert_At_Head(&head, 3);
     insert_At_Head(&head, 2);
-    insert_At_Head(&head, 7);
-    insert_At_Head(&head, 8);
-    insert_At_Head(&head, 9);
-
+    insert_At_Head(&head, 1);
     traverse_SLL_From_Head(head);
     
-    insert_At_Tail(&head, 3);
-
+    insert_At_Tail(&head, 7);
     traverse_SLL_From_Head(head);
 
-    insert_At_Position(&head, 6, 1);
-
+    insert_At_Position(&head, 6, 5);
     traverse_SLL_From_Head(head);
 
     int e = 3;
     if(search_Element_In_SLL(head, e)) {
-        printf("%i Found\n", e);
+        printf("%i found\n", e);
     } else {
-        printf("Not found\n");
+        printf("not found\n");
     }
 
     e = 2;
@@ -49,5 +46,6 @@ int main() {
     delete_At_Position(&head, 3);
     traverse_SLL_From_Head(head);
 
+    printf("Size of linked list after deletion: %i\n", get_Length_Of_SLL(head));
     return 0;
 }
