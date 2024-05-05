e09cfc23b96e7247855999ba3b3a5df5cd72069d
ClintNunez <clintnunez14@gmail.com>
2024-05-05 16:41:45 +0800

    additional test

--- data_structures/c/linkedlist/circular_singly_linked_main.c
+++ data_structures/c/linkedlist/circular_singly_linked_main.c
@@ -83,11 +83,21 @@ int main() {
     delete_Before_Node_CSLL(&tail1, 1);
     delete_After_Node_CSLL(&tail1, 1);
 
-    printf("Normal insert:\n");
+    printf("Deleting a node to make a one element list:\n");
+    insert_At_Head_CSLL(&tail1, 2);
     insert_At_Head_CSLL(&tail1, 1);
     traverse_CSLL_From_Tail(tail1);
 
-    printf("Delete single node to make list null:\n");
+    delete_Node_By_Data_CSLL(&tail1, 1);
+    traverse_CSLL_From_Tail(tail1);
+    
+    insert_At_Head_CSLL(&tail1, 1);
+    traverse_CSLL_From_Tail(tail1);
+
+    delete_Node_By_Data_CSLL(&tail1, 2);
+    traverse_CSLL_From_Tail(tail1);
+
+    printf("Delete last node to make list null:\n");
     delete_Head_CSLL(&tail1);
     traverse_CSLL_From_Tail(tail1);
 
