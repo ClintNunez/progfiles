7c133e92e643ad846fb03068aae7812c680fbd5e
ClintNunez <clintnunez14@gmail.com>
2024-05-05 18:31:15 +0800

    added more test and detailed output

--- data_structures/c/linkedlist/circular_doubly_linked_main.c
+++ data_structures/c/linkedlist/circular_doubly_linked_main.c
@@ -7,22 +7,33 @@
 int main() {
     struct cdll_node *head = NULL;    
 
+    printf("Insert at head:\n");
+    insert_At_Head_CDLL(&head, 4);
     insert_At_Head_CDLL(&head, 3);
     insert_At_Head_CDLL(&head, 2);
     insert_At_Head_CDLL(&head, 1);
     traverse_CDLL_From_Head(head);
 
-    insert_At_Tail_CDLL(&head, 4);
+    printf("Insert at head:\n");
     insert_At_Tail_CDLL(&head, 5);
     insert_At_Tail_CDLL(&head, 6);
     insert_At_Tail_CDLL(&head, 7);
+    insert_At_Tail_CDLL(&head, 8);
     traverse_CDLL_From_Head(head);
 
-    insert_Before_Node_CDLL(&head, 50, 3);
-    insert_After_Node_CDLL(&head, 100, 3);
+    printf("Insert before the given node:\n");
+    insert_Before_Node_CDLL(&head, 10, 5);
+    insert_Before_Node_CDLL(&head, 10, 1);
+    insert_Before_Node_CDLL(&head, 10, 8);
     traverse_CDLL_From_Head(head);
 
-    printf("length of cdll: %i\n", get_Length_Of_CDLL(head));
+    printf("Insert after the given node:\n");
+    insert_After_Node_CDLL(&head, 20, 5);
+    insert_After_Node_CDLL(&head, 20, 1);
+    insert_After_Node_CDLL(&head, 20, 8);
+    traverse_CDLL_From_Head(head);
+
+    printf("Length of cdll: %i\n", get_Length_Of_CDLL(head));
 
     int e = 1;
     if(search_Node_In_CDLL(head, e)) {
@@ -37,19 +48,54 @@ int main() {
     } else {
         printf("%i not found\n", e);
     }
-
+    
+    printf("Delete head:\n");
     delete_Head_CDLL(&head);
+    traverse_CDLL_From_Head(head);
+
+    printf("Delete tail:\n");
     delete_Tail_CDLL(&head);
     traverse_CDLL_From_Head(head);
 
+    printf("Delete using given data:\n");
     delete_Node_By_Data_CDLL(&head, 4);
     traverse_CDLL_From_Head(head);
     
+    printf("Delete befure and after given data:\n");
     delete_Before_Node_CDLL(&head, 3);
     delete_After_Node_CDLL(&head, 3);
     traverse_CDLL_From_Head(head);
 
-    printf("length of cdll after deletion: %i\n", get_Length_Of_CDLL(head));
+    printf("Length of cdll after deletion: %i\n", get_Length_Of_CDLL(head));
+
+    printf("Test for edge cases\n");
+    struct cdll_node *tail1 = NULL;
+    traverse_CDLL_From_Head(tail1);
+
+    printf("Delete function on empty list:\n");
+    delete_Head_CDLL(&tail1);
+    delete_Tail_CDLL(&tail1);
+    delete_Node_By_Data_CDLL(&tail1, 1);
+    delete_Before_Node_CDLL(&tail1, 1);
+    delete_After_Node_CDLL(&tail1, 1);
+
+    printf("Deleting a node to make a one element list:\n");
+    insert_At_Head_CDLL(&tail1, 2);
+    insert_At_Head_CDLL(&tail1, 1);
+    traverse_CDLL_From_Head(tail1);
+
+    delete_Node_By_Data_CDLL(&tail1, 1);
+    traverse_CDLL_From_Head(tail1);
+    
+    insert_At_Head_CDLL(&tail1, 1);
+    traverse_CDLL_From_Head(tail1);
+
+    delete_Node_By_Data_CDLL(&tail1, 2);
+    traverse_CDLL_From_Head(tail1);
+
+    printf("Delete last node to make list null:\n");
+    delete_Head_CDLL(&tail1);
+    traverse_CDLL_From_Head(tail1);
 
     return 0;
 }
