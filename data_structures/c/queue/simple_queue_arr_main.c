b754d58dd9234cb9f3e8431513bb07def0dc1760
ClintNunez <clintnunez14@gmail.com>
2024-05-12 16:33:17 +0800

    change function names

--- data_structures/c/queue/simple_queue_arr_main.c
+++ data_structures/c/queue/simple_queue_arr_main.c
@@ -6,17 +6,17 @@
 int main() {
     Array_Queue *arrQueue = new_Array_Queue(10);
     
-    enqueue_Array_Queue(arrQueue, 1);
-    enqueue_Array_Queue(arrQueue, 2);
-    enqueue_Array_Queue(arrQueue, 3);
+    enqueue_Array_Simple_Queue(arrQueue, 1);
+    enqueue_Array_Simple_Queue(arrQueue, 2);
+    enqueue_Array_Simple_Queue(arrQueue, 3);
 
-    int head = peek_Array_Queue(arrQueue), tail = rear_Array_Queue(arrQueue);
+    int head = peek_Array_Simple_Queue(arrQueue), tail = rear_Array_Simple_Queue(arrQueue);
 
-    int removedHead = dequeue_Array_Queue(arrQueue);
+    int removedHead = dequeue_Array_Simple_Queue(arrQueue);
 
     printf("head: %i, tail: %i, removedHead: %i\n", head, tail, removedHead);
 
-    print_Array_Queue(arrQueue);
+    print_Array_Simple_Queue(arrQueue);
 
     printf("new tail: %i\n", tail);
 
