b0fe25e2345ad471b2ea33fe8d533869c335ad99
ClintNunez <clintnunez14@gmail.com>
2024-05-19 09:54:51 +0800

    implementation of priority queue usng linked list

---
+++ data_structures/c/queue/descending_priority_QLL.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "custom_linked_list_queue_header.h"
+
+int main() {
+    Priority_QueueLL *priorityQLL = new_Priority_QLL();
+    
+    enqueue_Priority_Descending_QLL(priorityQLL, 3, 3);
+    print_Priority_QLL(priorityQLL);
+    enqueue_Priority_Descending_QLL(priorityQLL, 2, 2);
+    print_Priority_QLL(priorityQLL);
+    enqueue_Priority_Descending_QLL(priorityQLL, 1, 1);
+    print_Priority_QLL(priorityQLL);
+    enqueue_Priority_Descending_QLL(priorityQLL, 4, 2);
+    print_Priority_QLL(priorityQLL);
+    enqueue_Priority_Descending_QLL(priorityQLL, 6, 5);
+    print_Priority_QLL(priorityQLL);
+
+
+    int head = head_Priority_QLL(priorityQLL), tail = rear_Priority_QLL(priorityQLL);
+
+    int removedHead = dequeue_Priority_QLL(priorityQLL);
+
+    printf("head: %i, tail: %i, removedHead: %i\n", head, tail, removedHead);
+    
+    print_Priority_QLL(priorityQLL);
+
+    head = head_Priority_QLL(priorityQLL), tail = rear_Priority_QLL(priorityQLL);
+    printf("head: %i, tail: %i\n", head, tail);
+
+    return 0;
+}
