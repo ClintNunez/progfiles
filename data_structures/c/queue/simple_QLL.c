358b961566416997be7e66f5e785f31f60253e10
ClintNunez <clintnunez14@gmail.com>
2024-05-19 09:56:26 +0800

    minor fix

--- data_structures/c/queue/simple_QLL.c
+++ data_structures/c/queue/simple_QLL.c
@@ -6,15 +6,27 @@
 int main(int argc, char **argv) {
     QueueLL *queuell = new_QLL();
 
+    if(is_QLL_Empty(queuell)) {
+        printf("Queue empty\n");
+    } else {
+        printf("Queue is not empty\n");
+    }
+
     enqueue_QLL(queuell, 1);
     enqueue_QLL(queuell, 2);
+    enqueue_QLL(queuell, 3);
+    enqueue_QLL(queuell, 4);
+    enqueue_QLL(queuell, 5);
     print_QLL(queuell);
 
     int head = head_QLL(queuell), rear = rear_QLL(queuell);
     printf("head: %i, rear: %i\n", head, rear);
 
-    dequeue_QLL(queuell);
+    int dequeuedElement = dequeue_QLL(queuell);
+    printf("dequeuedElement: %i\n", dequeuedElement);
     print_QLL(queuell);
 
+    head = head_QLL(queuell);
+    printf("new head: %i\n", head);
     return 0;
 }
