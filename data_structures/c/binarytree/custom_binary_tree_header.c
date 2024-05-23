a38064cf08ba0c0707a33c1f8a4c14cb127c1533
ClintNunez <clintnunez14@gmail.com>
2024-05-23 21:36:21 +0800

    failed attempt for insert and traversal

--- data_structures/c/binarytree/custom_binary_tree_header.c
+++ data_structures/c/binarytree/custom_binary_tree_header.c
@@ -13,8 +13,17 @@ Node *new_Node(int data) {
     return newNode;
 }
 
+Node_Queue *new_Node_Queue(Node *node) {
+    Node_Queue *new_Node_Queue;
+    new_Node_Queue->nodePtr = node->left;
+    new_Node_Queue->next = NULL;
+
+    return new_Node_Queue;
+}
+
 void insert_Node(Node *root, int data) {
     if(root == NULL) {
+        printf("reached\n");
         root = new_Node(data);
     } else {
         /*
@@ -26,13 +35,69 @@ void insert_Node(Node *root, int data) {
 
         this algorithm makes sure that nodes that contains left or right nodes will be checked until there is a node that points to a null
         */
+        Node_Queue *node_Queue = malloc(sizeof(Node_Queue*));
+        node_Queue->nodePtr = root;
+        node_Queue->next = NULL;
+        Node *currentNode;
+        while(node_Queue != NULL) {
+            currentNode = node_Queue->nodePtr;
+            if(currentNode->left == NULL) {
+                currentNode->left = new_Node(data);
+                break;
+            } else {
+                node_Queue->next = new_Node_Queue(currentNode->left);
+            }
+            if(currentNode->right == NULL) {
+                currentNode->right = new_Node(data);
+                break;
+            } else {
+                node_Queue->next = new_Node_Queue(currentNode->right);
+            }
+            node_Queue = node_Queue->next;
+        }
     }
 }
 
-void delete_Node(Node *root, int key) {
+Node *delete_Deepest_Node(Node *root, int key) {
+
+}
+
+Node *delete_Node(Node *root, int key) {
+    /*
+    if(root == null) return null
+    if(root->left & root->right == null) if(data == key) return null else return root 
+
+    
+    
+     
+    */
     
 }
 
 void inorder_Traversal(Node *root) {
 
 } 
+
+void levelOrder_Traversal(Node *root) {
+    if(root != NULL) {
+        Node_Queue *node_Queue = malloc(sizeof(Node_Queue*));
+        node_Queue->nodePtr = root;
+        node_Queue->next = NULL;
+        Node *currentNode;
+
+        while(node_Queue != NULL) {
+            currentNode = node_Queue->nodePtr;
+            printf("%i ", currentNode->data);
+            if(currentNode->left) {
+                node_Queue->next = new_Node_Queue(currentNode->left);
+            }             
+            if(currentNode->right) {
+                node_Queue->next = new_Node_Queue(currentNode->right);
+            }             
+            node_Queue = node_Queue->next;
+        }
+        
+    } else {
+        printf("\tBinary tree empty\n");
+    }
+}
--- data_structures/c/binarytree/custom_binary_tree_header.h
+++ data_structures/c/binarytree/custom_binary_tree_header.h
@@ -6,13 +6,21 @@ typedef struct node {
     struct node *left, *right;
 } Node;
 
-Node *new_Node(int data);
-void insert_Node(Node *root, int data);
-void delete_Node(Node *root, int data);
+typedef struct node_queue{
+    Node *nodePtr;
+    struct node_queue *next;
+} Node_Queue;
 
+Node *new_Node(int data);
+Node_Queue *new_Node_Queue(Node *node);
+
+void insert_Node(Node *root, int data);
+Node *delete_Deepest_Node(Node *root, int data);
+Node *delete_Node(Node *root, int data);
+
+void levelOrder_Traversal(Node *root);
 void inorder_Traversal(Node *root);
 void preOrder_Traversal(Node *root);
 void postOrder_Traversal(Node *root);
-void levelOrder_Traversal(Node *root);
 
 #endif
