1b3592df6e789b9159ac097f80cca87404aa16e0
ClintNunez <clintnunez14@gmail.com>
2024-05-30 13:28:31 +0800

    added delete min/max and heapify down min/max

--- data_structures/c/heap/custom_heap_header.c
+++ data_structures/c/heap/custom_heap_header.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "custom_heap_header.h"
 
@@ -12,19 +13,30 @@ Heap *new_Heap(int length) {
     return newHeap;
 }
 
+int peek(Heap *heap_Ptr) {
+    return *(heap_Ptr->root);
+}
+
+void print_Heap(Heap *heap_Ptr) {
+    int *arr = heap_Ptr->root;
+    for(int i = 0; i < heap_Ptr->length; i++) {
+        printf("%i ", arr[i]);
+    }
+}
+
 Heap *heapify_Up_Max(Heap *heap_Ptr) {
     int *arr = heap_Ptr->root;
-    int currentIndex = heap_Ptr->length-1;
-    int parentIndex = (currentIndex - 1) / 2;
+    int current_Index = heap_Ptr->length-1;
+    int parentIndex = (current_Index - 1) / 2;
 
-    while(currentIndex != 0) {
-        if(arr[currentIndex] > arr[parentIndex]) {
-            int temp = arr[currentIndex];
-            arr[currentIndex] = arr[parentIndex];
+    while(current_Index != 0) {
+        if(arr[current_Index] > arr[parentIndex]) {
+            int temp = arr[current_Index];
+            arr[current_Index] = arr[parentIndex];
             arr[parentIndex] = temp;
             
-            currentIndex = parentIndex;
-            parentIndex = (currentIndex - 1) / 2;
+            current_Index = parentIndex;
+            parentIndex = (current_Index - 1) / 2;
         } else {
             break;
         }
@@ -35,17 +47,17 @@ Heap *heapify_Up_Max(Heap *heap_Ptr) {
 
 Heap *heapify_Up_Min(Heap *heap_Ptr) {
     int *arr = heap_Ptr->root;
-    int currentIndex = heap_Ptr->length-1;
-    int parentIndex = (currentIndex - 1) / 2;
+    int current_Index = heap_Ptr->length-1;
+    int parentIndex = (current_Index - 1) / 2;
 
-    while(currentIndex != 0) {
-        if(arr[currentIndex] < arr[parentIndex]) {
-            int temp = arr[currentIndex];
-            arr[currentIndex] = arr[parentIndex];
+    while(current_Index != 0) {
+        if(arr[current_Index] < arr[parentIndex]) {
+            int temp = arr[current_Index];
+            arr[current_Index] = arr[parentIndex];
             arr[parentIndex] = temp;
             
-            currentIndex = parentIndex;
-            parentIndex = (currentIndex - 1) / 2;
+            current_Index = parentIndex;
+            parentIndex = (current_Index - 1) / 2;
         } else {
             break;
         }
@@ -53,6 +65,8 @@ Heap *heapify_Up_Min(Heap *heap_Ptr) {
 
     return heap_Ptr;
 }
+
+
 
 Heap *insert_Node_Max(Heap *heap_Ptr, int data) {
     if(heap_Ptr->length == heap_Ptr->max_Length) {
@@ -82,20 +96,122 @@ Heap *insert_Node_Min(Heap *heap_Ptr, int data) {
     return heap_Ptr;
 }
 
-/*
-
-Heap *heapify_Down_Max(Heap *heap_Ptr) {
-
-}
-
-Heap *heapify_Down_Min(Heap *heap_Ptr) {
-    
-}
-*/
-
-void print_Heap(Heap *heap_Ptr) {
+void heapify_Down_Max(Heap *heap_Ptr, int root_Index) {
     int *arr = heap_Ptr->root;
-    for(int i = 0; i < heap_Ptr->length; i++) {
-        printf("%i ", arr[i]);
+    int current_Index = root_Index;
+    int left_Child_Index = (2 * current_Index) + 1;
+    int right_Child_Index = (2 * current_Index) + 2;
+    int temp;
+
+    if(left_Child_Index <= heap_Ptr->length - 1 && arr[left_Child_Index] > arr[current_Index]) {
+        temp = arr[left_Child_Index];
+        arr[left_Child_Index] = arr[current_Index];
+        arr[current_Index] = temp;
+
+        current_Index = left_Child_Index;
+        left_Child_Index = (2 * current_Index) + 1;
+        right_Child_Index = (2 * current_Index) + 2;
+
+        heapify_Down_Max(heap_Ptr, root_Index);
+    } 
+
+    if(right_Child_Index <= heap_Ptr->length - 1 && arr[right_Child_Index] > arr[current_Index]) {
+        temp = arr[right_Child_Index];
+        arr[right_Child_Index] = arr[current_Index];
+        arr[current_Index] = temp; 
+
+        current_Index = right_Child_Index;
+        left_Child_Index = (2 * current_Index) + 1;
+        right_Child_Index = (2 * current_Index) + 2;
+
+        heapify_Down_Max(heap_Ptr, root_Index);
     }
+
+    return; 
 }
+
+void heapify_Down_Min(Heap *heap_Ptr, int root_Index) {
+    int *arr = heap_Ptr->root;
+    int left_Child_Index = (2 * root_Index) + 1;
+    int right_Child_Index = (2 * root_Index) + 2;
+    int temp;
+
+    if(left_Child_Index <= heap_Ptr->length - 1 && arr[left_Child_Index] < arr[root_Index]) { 
+        temp = arr[left_Child_Index];
+        arr[left_Child_Index] = arr[root_Index];
+        arr[root_Index] = temp;
+
+        root_Index = left_Child_Index;
+        left_Child_Index = (2 * root_Index) + 1;
+        right_Child_Index = (2 * root_Index) + 2;
+
+        heapify_Down_Min(heap_Ptr, root_Index);
+    } 
+
+    if(right_Child_Index <= heap_Ptr->length - 1 && arr[right_Child_Index] < arr[root_Index]) {
+        temp = arr[right_Child_Index];
+        arr[right_Child_Index] = arr[root_Index];
+        arr[root_Index] = temp; 
+
+        root_Index = right_Child_Index;
+        left_Child_Index = (2 * root_Index) + 1;
+        right_Child_Index = (2 * root_Index) + 2;
+
+        heapify_Down_Min(heap_Ptr, root_Index);
+    }
+
+    return;
+}
+
+Heap *delete_Root_Node_Max(Heap *heap_Ptr) {
+    if(heap_Ptr->length == 0) {
+        printf("Heap empty\n");
+        return heap_Ptr;
+    }
+
+    if(heap_Ptr->length == 1) {
+        heap_Ptr->length--;
+        return heap_Ptr;
+    }
+
+    int *arr = heap_Ptr->root;
+
+    for(int i = 0; i <= heap_Ptr->length - 2; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    heap_Ptr->length--;
+
+    for(int i = 0; i <= (heap_Ptr->length / 2) - 1; i++) {
+        heapify_Down_Max(heap_Ptr, i);
+    }
+
+    return heap_Ptr;
+}
+
+Heap *delete_Root_Node_Min(Heap *heap_Ptr) {
+    if(heap_Ptr->length == 0) {
+        printf("Heap empty\n");
+        return heap_Ptr;
+    }
+
+    if(heap_Ptr->length == 1) {
+        heap_Ptr->length--;
+        return heap_Ptr;
+    }
+
+    int *arr = heap_Ptr->root;
+
+    for(int i = 0; i <= heap_Ptr->length - 2; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    heap_Ptr->length--;
+
+    for(int i = 0; i <= (heap_Ptr->length / 2) - 1; i++) {
+        heapify_Down_Min(heap_Ptr, i);
+    }
+
+    return heap_Ptr;
+}
+
--- data_structures/c/heap/heap_main.c
+++ data_structures/c/heap/heap_main.c
@@ -13,7 +13,16 @@ int main(int argc, char **argv) {
     insert_Node_Max(heap_Ptr_Max, 2);
     insert_Node_Max(heap_Ptr_Max, 13);
 
+    printf("\nRoot of max heap: %i", peek(heap_Ptr_Max));
+    
     printf("\nPrint max heap: ");
+    print_Heap(heap_Ptr_Max);
+
+    delete_Root_Node_Max(heap_Ptr_Max);
+
+    printf("\nRoot of max heap after deletion: %i", peek(heap_Ptr_Max));
+
+    printf("\nPrint max heap after deletion: ");
     print_Heap(heap_Ptr_Max);
 
     Heap *heap_Ptr_Min = new_Heap(8);
@@ -21,14 +30,22 @@ int main(int argc, char **argv) {
     insert_Node_Min(heap_Ptr_Min, 1);
     insert_Node_Min(heap_Ptr_Min, 5);
     insert_Node_Min(heap_Ptr_Min, 6);
-    insert_Node_Min(heap_Ptr_Min, 2);
-    insert_Node_Min(heap_Ptr_Min, 4);
     insert_Node_Min(heap_Ptr_Min, 3);
+    insert_Node_Min(heap_Ptr_Min, 4);
+    insert_Node_Min(heap_Ptr_Min, 2);
     insert_Node_Min(heap_Ptr_Min, 8);
+
+    printf("\nRoot of min heap: %i", peek(heap_Ptr_Min));
 
     printf("\nPrint min heap: ");
     print_Heap(heap_Ptr_Min);
 
+    delete_Root_Node_Min(heap_Ptr_Min);
+
+    printf("\nRoot of min heap after deletion: %i", peek(heap_Ptr_Min));
+
+    printf("\nPrint min heap after deletion: ");
+    print_Heap(heap_Ptr_Min);
 
     return 0;
 }
