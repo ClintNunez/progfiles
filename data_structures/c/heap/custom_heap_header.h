8bb5bbb28212c60c5c0e1a17efb2e0d336acdc65
ClintNunez <clintnunez14@gmail.com>
2024-05-30 13:27:36 +0800

    minor fix

--- data_structures/c/heap/custom_heap_header.h
+++ data_structures/c/heap/custom_heap_header.h
@@ -7,14 +7,15 @@ typedef struct heap {
 
 Heap *new_Heap(int length);
 
-Heap *insert_Node_Max(Heap *heap_Ptr, int data);
-Heap *insert_Node_Min(Heap *heap_Ptr, int data);
-
-Heap *delete_Root_Node_Max(Heap *heap_Ptr, int data);
-Heap *delete_Root_Node_Min(Heap *heap_Ptr, int data);
-
 int peek(Heap *heap_Ptr);
 
 void print_Heap(Heap *heap_Ptr);
+
+Heap *insert_Node_Max(Heap *heap_Ptr, int data);
+Heap *insert_Node_Min(Heap *heap_Ptr, int data);
+
+Heap *delete_Root_Node_Max(Heap *heap_Ptr);
+Heap *delete_Root_Node_Min(Heap *heap_Ptr);
+
 
 #endif
