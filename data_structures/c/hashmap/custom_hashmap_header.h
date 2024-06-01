371f4d226e1309b67427ebc2a5b33626059b3152
ClintNunez <clintnunez14@gmail.com>
2024-06-01 18:27:28 +0800

    header file setup

---
+++ data_structures/c/hashmap/custom_hashmap_header.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOM_HASHMAP_HEADER
+#define CUSTOM_HASHMAP_HEADER
+
+typedef struct node {
+    char *key;
+    int value;
+    struct node *next;
+} Node;
+
+typedef struct Hashmap {
+    Node **buckets;
+    int size;
+} Hashmap;
+
+Hashmap *new_Hashmap(int size);
+unsigned int hash(const char *key, int size);
+void insert_Node(Hashmap *hashmap, const char *key, int value);
+int search_Node(Hashmap *hashmap, const char *key, int value);
+void delete_Node(Hashmap *hashmap, const char *key);
+void free_Hashmap(Hashmap *hashmap);
+
+#endif
