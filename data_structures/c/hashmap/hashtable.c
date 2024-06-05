15ba7007838f2b72c2fc2d5f735c26bacfd8fa69
ClintNunez <clintnunez14@gmail.com>
2024-06-05 19:01:54 +0800

    added get and set functions

--- data_structures/c/hashmap/hashtable.c
+++ data_structures/c/hashmap/hashtable.c
@@ -1,6 +1,8 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "hashtable.h"
 
@@ -35,11 +37,132 @@ Hashtable *create_hashtable() {
     return hashtable;
 };
 
-void destroy_hashtable(Hashtable *hashtable);
+void destroy_hashtable(Hashtable *hashtable) {
+    for(size_t i = 0; i < hashtable->capacity; i++) {
+        free((void *)hashtable->hashtable_items[i].key);
+    }
 
-void *get_item(Hashtable *hashtable, const char *key);
 
-const char *set_item(Hashtable *hashtable, const char *key, void *value);
+    free(hashtable->hashtable_items);
+    free(hashtable);
+};
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+// Return 64-bit FNV-1a hash for key (NUL-terminated). See description:
+// https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
+static uint64_t hash_key(const char* key) {
+    uint64_t hash = FNV_OFFSET;
+    for (const char* p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
+
+void *get_item(Hashtable *hashtable, const char *key) {
+    // hashkey
+    uint64_t hash = hash_key(key);
+    // get index by ANDing capacity - 1
+    size_t index = (size_t)(hash & hashtable->capacity - 1);
+    //size_t index = (size_t)(hash & (uint64_t)(hashtable->capacity - 1));
+    
+    // loop through the items
+    while(hashtable->hashtable_items[index].key != NULL) {
+        // compare keys, return value if keys match
+        if(strcmp(key, hashtable->hashtable_items[index].key) == 0) {
+            return hashtable->hashtable_items[index].value; 
+        }
+
+        // linear probing
+        index++;
+
+        // wrap arround if index exceed capacity
+        if(index > hashtable->capacity) {
+            index = 0;
+        }
+    }
+    
+    return NULL;
+}
+
+// helper functions for set 
+static const char* hashtable_set_item(Hashtable_item *hashtable_items, size_t capacity, const char *key, void *value, size_t *plength) {
+    uint64_t hash = hash_key(key);
+    size_t index = (size_t)(hash & (uint64_t)(capacity - 1));
+
+    // loop till we find an empty slot
+    while(hashtable_items[index].key != NULL) {
+        // update value of key if it already exists
+        if(strcmp(key, hashtable_items[index].key) == 0) {
+            hashtable_items[index].value = value;
+            return hashtable_items[index].key;
+        }
+
+        index++;
+        if(index >= capacity) {
+            index = 0;
+        }
+    }
+    
+
+    // add item to the array if it doesn't exist
+    if(plength != NULL) {
+        key = strdup(key);
+        if(key == NULL) {
+            return NULL;
+        }
+        (*plength)++;
+    }
+
+    hashtable_items[index].key = key;
+    hashtable_items[index].value = value;
+    return key;
+}
+
+static bool hashtable_expand(Hashtable *hashtable) {
+    // allocate new entries 
+    size_t new_capacity = hashtable->capacity * 2;
+
+    // overflow
+    if (new_capacity < hashtable->capacity) {
+        return false;
+    }
+    Hashtable_item *new_hashtable_items = calloc(new_capacity, sizeof(Hashtable_item));
+    if (new_hashtable_items == NULL) {
+        return NULL;
+    }
+
+    // loop through items and move all non empty items to a new array
+    for (size_t i = 0; i < hashtable->capacity; i++) {
+        Hashtable_item hashtable_item = hashtable->hashtable_items[i];
+        if(hashtable_item.key != NULL) {
+            hashtable_set_item(new_hashtable_items, new_capacity, 
+                               hashtable_item.key, hashtable_item.value, NULL);
+        }
+    }
+    
+    // free old table and update values 
+    free(hashtable->hashtable_items);
+    hashtable->hashtable_items = new_hashtable_items;
+    hashtable->capacity = new_capacity;
+    return true;
+}
+
+const char *set_item(Hashtable *hashtable, const char *key, void *value) {
+    if(value == NULL) {
+        return NULL;
+    }
+
+    if(hashtable->length >= hashtable->capacity / 2) {
+        if(!hashtable_expand(hashtable)) {
+            return NULL;
+        }
+    }
+    
+    return hashtable_set_item(hashtable->hashtable_items, hashtable->capacity, key, value, &hashtable->length);
+}
 
 size_t hashtable_length(Hashtable *hashtable);
 
