587d146ce16a9bc7b4d3d321e5acfd864eba07c3
ClintNunez <clintnunez14@gmail.com>
2024-07-09 10:13:16 +0800

    added given optimized solution

--- data_structures/c/array/exercises/easy1.c
+++ data_structures/c/array/exercises/easy1.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void RotateLeft(int arr[], int nRotation) {
+void rotateLeft(int arr[], int nRotation) {
     int temp;
     for(int i = 0; i < nRotation; i++) {
         temp = arr[0];
@@ -26,6 +26,23 @@ void copyArr(int arr[], int tempArr[]) {
     }
 }
 
+/*
+* Given solution:
+* uses modulo operation to get the starting index
+* and prints the array from there.
+*
+* Saves space by manipulating the index instead
+* of manipulating a copy of the given array.
+*/
+void rotateLeftOpt(int arr[], int nRotation) {
+    int mod = nRotation % 5;
+        
+    for(int i = 0; i < 5; i++) {
+        printf("%i ", arr[(mod + i) % 5]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char ** argv) {
     int arr[5], i;
 
@@ -39,11 +56,16 @@ int main(int argc, char ** argv) {
         arrRotation[j] = atoi(argv[i]);
     }
 
+    printf("Rotating a copy of given array:\n");
     for(i = 0; i < nRotationArgs; i++) {
         int tempArr[5];
         copyArr(arr, tempArr);
-        RotateLeft(tempArr, arrRotation[i]);
+        rotateLeft(tempArr, arrRotation[i]);
     }
 
+    printf("\nShifting the starting index before print:\n");
+    for(i = 0; i < nRotationArgs; i++) {
+        rotateLeftOpt(arr, arrRotation[i]);
+    }
     return 0;
 } 
